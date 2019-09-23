e695c7121d5d6be910fba4bf253a6f662c575fa8
Sami Väisänen <samiv@ensisoft.com>
2019-09-23 18:52:41 +0300

    Use smoothstep to rasterize concentric rings effect
    Use smoothstep to rasterize concentric rings effect instead of discard.
    This helps with the antialiasing and produces smoother better looking
    effect.

--- game/dist/shaders/es2/concentric_rings_effect.glsl
+++ game/dist/shaders/es2/concentric_rings_effect.glsl
@@ -6,6 +6,13 @@ uniform float uRuntime;
 uniform vec4 kBaseColor;
 varying vec2 vTexCoord;
 
+float ring_alpha(float ring_distance_from_origin, float fragment_distance_from_origin)
+{
+    float ring_width = 0.005;    
+    float rd = ring_distance_from_origin;
+    float fd = fragment_distance_from_origin;
+    return smoothstep(rd - ring_width, rd, fd) - smoothstep(rd, rd + ring_width, fd);
+}
 
 void main()
 {
@@ -17,22 +24,17 @@ void main()
     rings[1] = 0.08 + s;
     rings[2] = 0.16 + s;
     rings[3] = 0.24 + s;
-    float ring_width = 0.005;
 
     vec2 pos   = vTexCoord - vec2(0.5, 0.5);
     vec2 ori   = vec2(0.0, 0.0);
     float dist = length(pos - ori);
 
-    bool found = false;
-    for (int i=0; i<4; ++i) {
-        if (dist >= rings[i] && dist <= rings[i] + ring_width) {
-            found = true;
-            break;
-        }
-    }
-    if (!found)
-       discard;
+    float ring_alpha0 = ring_alpha(rings[0], dist);
+    float ring_alpha1 = ring_alpha(rings[1], dist);
+    float ring_alpha2 = ring_alpha(rings[2], dist);
+    float ring_alpha3 = ring_alpha(rings[3], dist);
+    float any_ring_alpha = ring_alpha0 + ring_alpha1 + ring_alpha2 + ring_alpha3;
 
-    float alpha = 1.0 - dist/ 0.5 * 1.0;
-    gl_FragColor = kBaseColor * vec4(1.0, 1.0, 1.0, alpha);
+    float distance_alpha = 1.0 - dist/ 0.5 * 1.0;
+    gl_FragColor = kBaseColor * vec4(1.0, 1.0, 1.0, distance_alpha * any_ring_alpha);
 }
