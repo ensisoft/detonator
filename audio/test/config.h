3326d09b48e33ddf5524273cfa7a0b80c2a9391e
Sami Väisänen <samiv@ensisoft.com>
2019-12-30 19:16:14 +0200

    Make config.h files unique per each build target
    Make config.h files unique per each build target.

---
+++ audio/test/config.h
@@ -0,0 +1,6 @@
+
+// build time configuration options for the audio test app
+
+#include "base/platform.h"
+
+// currently intentionally empty
---
+++ graphics/unit_test/config.h
@@ -0,0 +1,6 @@
+
+// build time configuration options for the unit tests
+
+// currently intentionally empty
+
+#include "base/platform.h"
