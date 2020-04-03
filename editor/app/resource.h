ensisoft/detonator

commit 27c5b874faab75cb1d2ba76ecd75d613f7d3989a

Add tracking of gfx resource instances in the workspace
Add tracking of gfx resource instances in the workspace so that if
a resource is modified and saved any objects using the resource
will also reflect the changes.

editor/app/resource.h
@@ -140,6 +140,8 @@ namespace app
     class GraphicsResource : public Resource
     {
     public:
+        using GfxType = Content;
+
         GraphicsResource(const Content& content, const QString& name)
             : mContent(content)
             , mName(name)
@@ -225,6 +227,51 @@ namespace app
         QString mName;
         QVariantMap mProps;
     };
+
+    // Resource handle wraps a gfx resource object
+    // and provides some additional functionality that pertains
+    // only to the *instance*. Not the resource type per se.
+    class ResourceHandle
+    {
+    public:
+        virtual ~ResourceHandle() = default;
+        // Update the instance data, for example material parameters from a gfx::Material object.
+        virtual void UpdateInstance(const QString& name, Resource::Type type, const void* gfx_content_source) = 0;
+        // Checks if the handle has expired.
+        virtual bool IsExpired() const = 0;
+        // Return the name of the resource that this instance represents..
+        virtual QString GetName() const = 0;
+    };
+
+    template<typename Content, Resource::Type type>
+    class WeakGraphicsResourceHandle : public ResourceHandle
+    {
+    public:
+        WeakGraphicsResourceHandle(const QString& name, std::shared_ptr<Content> handle)
+            : mName(name)
+            , mHandle(handle)
+        {}
+        virtual void UpdateInstance(const QString& name, Resource::Type source_type, const void* gfx_content_source) override
+        {
+            if (type != source_type)
+                return;
+            else if (mName != name)
+                return;
+            auto ptr = mHandle.lock();
+            if (!ptr)
+                return;
+            *ptr = *static_cast<const Content*>(gfx_content_source);
+        }
+        virtual bool IsExpired() const override
+        { return mHandle.expired(); }
+        virtual QString GetName() const override
+        { return mName; }
+
+    private:
+        const QString mName;
+        std::weak_ptr<Content> mHandle;
+    };
+
 
     using MaterialResource = GraphicsResource<gfx::Material, Resource::Type::Material>;
     using ParticleSystemResource = GraphicsResource<gfx::KinematicsParticleEngine,

editor/app/workspace.h
@@ -80,18 +80,34 @@ namespace app
         // the workspace.
         template<typename ResourceType>
         void SaveResource(const ResourceType& resource)
-        {
+        {                  
+            const typename ResourceType::GfxType* src = nullptr;
+            ASSERT(resource.GetContent(&src));
+
             const auto& name = resource.GetName();
             const auto  type = resource.GetType();
             for (size_t i=0; i<mResources.size(); ++i)
             {
-                const auto& res = mResources[i];
+                auto& res = mResources[i];
                 if (res->GetName() != name || res->GetType() != type)
                     continue;
 
                 // overwrite the existing resource type. the caller
                 // is expected to have confirmed with the user that this is OK
-                mResources[i] = std::make_unique<ResourceType>(resource);
+                typename ResourceType::GfxType* dst = nullptr;
+                ASSERT(res->GetContent(&dst));
+                // we can either assign to the content object in the existing
+                // resource object or create a new resource object
+                *dst = *src;
+                //mResources[i] = std::make_unique<ResourceType>(resource);
+
+                // see if there are instances of this resource in use that we
+                // also want to update with the latest changes.
+                // note that there can be multiple handles here!
+                for (auto& handle : mPrivateInstances)
+                {
+                    handle->UpdateInstance(name, type, (const void*)src);
+                }
                 return;
             }
             beginInsertRows(QModelIndex(), mResources.size(), mResources.size());
@@ -133,8 +149,21 @@ namespace app
 
         void DeleteResources(QModelIndexList& list);
 
+        void Tick();
+
     private:
+        // this is the list of resources that we save/load
+        // from the workspace data files. these are created
+        // and edited by the user through the editor.
         std::vector<std::unique_ptr<Resource>> mResources;
+        // we keep track of private instances of resource objects
+        // through non-owning pointer.
+        // such instances came into life when resources of other
+        // types use other resources, for example when an animation
+        // uses a material but the reason why we track them here
+        // is that we can reflect the changes done to the resources
+        // in the system that uses it!
+        mutable std::vector<std::unique_ptr<ResourceHandle>> mPrivateInstances;
     private:
         mutable QString mContentFile;
         mutable QString mWorkspaceFile;

editor/app/workspace.cpp
@@ -110,7 +110,15 @@ std::shared_ptr<gfx::Material> Workspace::MakeMaterial(const std::string& name) 
     const Resource& resource = GetResource(FromUtf8(name), Resource::Type::Material);
     const gfx::Material* content = nullptr;
     resource.GetContent(&content);
-    return std::make_shared<gfx::Material>(*content);
+    auto ret = std::make_shared<gfx::Material>(*content);
+
+    // add a copy in the collection of private instances so that
+    // if/when the resource is modified the object using the resource
+    // will also reflect those changes.
+    auto handle = std::make_unique<WeakGraphicsResourceHandle<gfx::Material, Resource::Type::Material>>(FromUtf8(name), ret);
+    mPrivateInstances.push_back(std::move(handle));
+
+    return ret;
 }
 std::shared_ptr<gfx::Drawable> Workspace::MakeDrawable(const std::string& name) const
 {
@@ -120,7 +128,15 @@ std::shared_ptr<gfx::Drawable> Workspace::MakeDrawable(const std::string& name) 
         const Resource& resource = GetResource(FromUtf8(name), Resource::Type::ParticleSystem);
         const gfx::KinematicsParticleEngine* engine = nullptr;
         resource.GetContent(&engine);
-        return std::make_shared<gfx::KinematicsParticleEngine>(*engine);
+        auto ret = std::make_shared<gfx::KinematicsParticleEngine>(*engine);
+
+        // add a copy in the collection of private instances so that
+        // if/when the resource is modified the object using the resource
+        // will also reflect those changes.
+        auto handle = std::make_unique<WeakGraphicsResourceHandle<gfx::KinematicsParticleEngine, Resource::Type::ParticleSystem>>(FromUtf8(name), ret);
+        mPrivateInstances.push_back(std::move(handle));
+
+        return ret;
     }
 
     ERROR("Request for a drawable that doesn't exist: '%1'", name);
@@ -358,6 +374,28 @@ void Workspace::DeleteResources(QModelIndexList& list)
 
         endRemoveRows();
         ++removed;
+    }
+}
+
+void Workspace::Tick()
+{
+    // cleanup expired handles that point to stale objects that
+    // are no longer actually referenced.
+    for (size_t i=0; i<mPrivateInstances.size(); )
+    {
+        if (mPrivateInstances[i]->IsExpired())
+        {
+            const auto name = mPrivateInstances[i]->GetName();
+            const auto last = mPrivateInstances.size() - 1;
+            std::swap(mPrivateInstances[i], mPrivateInstances[last]);
+            mPrivateInstances.pop_back();
+            DEBUG("Deleted resource tracking handle %1", name);
+        }
+        else
+        {
+            ++i;
+        }
+
     }
 }
 

editor/gui/mainwindow.cpp
@@ -449,6 +449,8 @@ void MainWindow::refreshUI()
         mUI.mainTab->setTabText(i, text);
         mUI.mainTab->setTabIcon(i, icon);
     }
+
+    mWorkspace.Tick();
 }
 
 void MainWindow::showNote(const app::Event& event)

graphics/drawable.h
@@ -91,7 +91,7 @@ namespace gfx
                     {{0.0f * mWidth, -0.25f * mHeight}, {0.0f, 0.75f}},
                     {{0.0f * mWidth, -0.75f * mHeight}, {0.0f, 0.25f}},
                     {{0.7f * mWidth, -0.25f * mHeight}, {0.7f, 0.75f}},
-                    // body 
+                    // body
                     {{0.7f * mWidth, -0.25f * mHeight}, {0.7f, 0.75f}},
                     {{0.0f * mWidth, -0.75f * mHeight}, {0.0f, 0.25f}},
                     {{0.7f * mWidth, -0.75f * mHeight}, {0.7f, 0.25f}},
@@ -120,7 +120,7 @@ namespace gfx
     class Circle : public Drawable
     {
     public:
-        Circle() = default; 
+        Circle() = default;
         Circle(const std::string& name, float radius)
             : mName(name)
             , mRadius(radius)
@@ -641,7 +641,7 @@ namespace gfx
         }
 
     private:
-        const Params mParams;
+        /* const */ Params mParams;
         float mNumParticlesHatching = 0.0f;
         float mTime = 0.0f;
         std::vector<Particle> mParticles;

commit 8c2db75e6dbeb3f1a66f4d9a60255aaaebc94229

Fix a bug introduced in commit 27c5b874faab75cb1d2ba76ecd75d613f7d3989a
Fix a bug introduced in commit 27c5b874faab75cb1d2ba76ecd75d613f7d3989a.
That commit changed Workspace::SaveResource to mistakenly only copy
the contained object instead of the resource object itself into the
workspace. We need to copy the whole resource object in order to
also copy the properties !

editor/app/resource.h
@@ -236,6 +236,10 @@ namespace app
             else if (TypeValue == Resource::Type::Animation)
                 mProps = object["animation_" + mName].toObject().toVariantMap();
         }
+        Content* GetContent()
+        { return &mContent; }
+        const Content* GetContent() const
+        { return &mContent; }
 
     protected:
         virtual void* GetIf(const std::type_info& expected_type) override

editor/app/workspace.h
@@ -93,12 +93,9 @@ namespace app
         // Save a new resource in the workspace. If the resource by the same type
         // and name exists it's overwritten, otherwise a new resource is added to
         // the workspace.
-        template<typename ResourceType>
-        void SaveResource(const ResourceType& resource)
+        template<typename T>
+        void SaveResource(const GraphicsResource<T>& resource)
         {
-            const typename ResourceType::GfxType* src = nullptr;
-            ASSERT(resource.GetContent(&src));
-
             const auto& name = resource.GetName();
             const auto  type = resource.GetType();
             for (size_t i=0; i<mResources.size(); ++i)
@@ -109,24 +106,28 @@ namespace app
 
                 // overwrite the existing resource type. the caller
                 // is expected to have confirmed with the user that this is OK
-                typename ResourceType::GfxType* dst = nullptr;
-                ASSERT(res->GetContent(&dst));
-                // we can either assign to the content object in the existing
-                // resource object or create a new resource object
-                *dst = *src;
-                //mResources[i] = std::make_unique<ResourceType>(resource);
+                // note that we must make sure to not only update the
+                // contained resource parameters but also the application level
+                // resource object and since that is a polymorphic type
+                // we must allocate new resource object.
+                mResources[i] = std::make_unique<GraphicsResource<T>>(resource);
 
                 // see if there are instances of this resource in use that we
                 // also want to update with the latest changes.
                 // note that there can be multiple handles here!
+                // here we're only interested in updating the gfx object content (parameters)
+                const auto* source = resource.GetContent();
+
                 for (auto& handle : mPrivateInstances)
                 {
-                    handle->UpdateInstance(name, type, (const void*)src);
+                    handle->UpdateInstance(name, type, (const void*)source);
                 }
                 return;
             }
+            // if we're here no such resource exists yet.
+            // Create a new resource and add it to the list of resources.
             beginInsertRows(QModelIndex(), mResources.size(), mResources.size());
-            mResources.push_back(std::make_unique<ResourceType>(std::move(resource)));
+            mResources.push_back(std::make_unique<GraphicsResource<T>>(resource));
             endInsertRows();
         }
 