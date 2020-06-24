ensisoft/detonator

commit 3fc0215ff5f3e066abe1788efa32dd2da026fa32

Refactor the animation node transformation and object position
Refector the animation node transformation and object position so
that the object's position is now with respect to the shape's center.
I.e. the center point of the shape aligns with the position of the
node/shape.

This change arguably makes the rest of the node's transformations
more intuitive to the user. Alternative is to use the corner as
was done before but then the correlation between position is kinda
lost when the object is rotated, unless the rotation is around the
origin of the shape (it's corner) but this feels rather un-natural.

Change the animation widget tools to respect the new definition for
node's position (translation) parameter.

editor/gui/animationwidget.cpp
@@ -196,7 +196,8 @@ public:
         node.SetMaterial(app::ToUtf8(mMaterialName), mMaterial);
         node.SetDrawable(app::ToUtf8(mDrawableName), mDrawable);
         node.SetName(name);
-        node.SetTranslation(glm::vec2(xpos, ypos));
+        // the given object position is to be aligned with the center of the shape
+        node.SetTranslation(glm::vec2(xpos + 0.5*width, ypos + 0.5*height));
         node.SetSize(glm::vec2(width, height));
         node.SetScale(glm::vec2(1.0f, 1.0f));
 
@@ -349,8 +350,12 @@ public:
         const auto& widget_to_view = glm::inverse(trans.GetAsMatrix());
         const auto& mouse_pos_in_view = widget_to_view * glm::vec4(mouse_pos.x(), mouse_pos.y(), 1.0f, 1.0f);
 
-        const auto& mouse_delta = mouse_pos_in_view - mPreviousMousePos;
-        //DEBUG("Mouse delta %1,%2", mouse_delta.x, mouse_delta.y);
+        // double the mouse movement so that the object's size follows the actual mouse movement.
+        // Since the object's position is with respect to the center of the shape
+        // adding some delta d to any extent (width or height i.e dx or dy) will
+        // only grow that dimension by half d on either side of the axis, thus
+        // falling behind the actual mouse movement.
+        const auto& mouse_delta = (mouse_pos_in_view - mPreviousMousePos) * 2.0f;
 
         const bool maintain_aspect_ratio = mickey->modifiers() & Qt::ControlModifier;
         if (maintain_aspect_ratio)

scene/animation.cpp
@@ -78,9 +78,10 @@ glm::mat4 AnimationNode::GetNodeTransform() const
     // transformation order is the order they're
     // written here.
     gfx::Transform transform;
+    // offset the object so that the center of the shape is aligned
+    // with the position parameter.
     transform.Translate(-mSize.x * 0.5f, -mSize.y * 0.5f);
     transform.Rotate(mRotation);
-    transform.Translate(mSize.x * 0.5f, mSize.y * 0.5f);
     transform.Translate(mPosition);
     return transform.GetAsMatrix();
 }

commit e70ba5d6bbe93fc7f5558523f1751bd0dbf9ff62

Fix a bug introduced in 3fc0215ff5f3e066abe1788efa32dd2da026fa32
Fix a bug introduced in 3fc0215ff5f3e066abe1788efa32dd2da026fa32.
In that change AnimationNode implementation was changed so that the
drawable shape's center would be aligned around the node's position.
However this translate was added to the node's transformation instead
of the node's drawable (model) transform. This means that the offset
would be carried over to child nodes as well, i.e. their position
0, 0, would no longer map to the center of the parent node.

This commit moves the translation from node's transformation to the
node's drawable (model) transformation.

editor/gui/animationwidget.cpp
@@ -351,7 +351,12 @@ public:
         const auto& mouse_pos_in_view = widget_to_view * glm::vec4(mouse_pos.x(), mouse_pos.y(), 1.0f, 1.0f);
         const auto& mouse_pos_in_node = mState.animation.MapCoordsToNode(mouse_pos_in_view.x, mouse_pos_in_view.y, mNode);
 
-        const auto& mouse_delta = (mouse_pos_in_node - mPreviousMousePos);
+        // double the mouse movement so that the object's size follows the actual mouse movement.
+        // Since the object's position is with respect to the center of the shape
+        // adding some delta d to any extent (width or height i.e dx or dy) will
+        // only grow that dimension by half d on either side of the axis, thus
+        // falling behind the actual mouse movement.
+        const auto& mouse_delta = (mouse_pos_in_node - mPreviousMousePos) * 2.0f;
 
         const bool maintain_aspect_ratio = mickey->modifiers() & Qt::ControlModifier;
         if (maintain_aspect_ratio)
@@ -580,8 +585,7 @@ AnimationWidget::AnimationWidget(app::Workspace* workspace)
                     mCurrentTool.reset(new ResizeTool(mState, hit));
                 else if (top_left_hitbox_hit)
                     mCurrentTool.reset(new RotateTool(mState, hit));
-                else
-                    mCurrentTool.reset(new MoveTool(mState, hit));
+                else mCurrentTool.reset(new MoveTool(mState, hit));
 
                 mUI.tree->SelectItemById(app::FromUtf8(hit->GetId()));
             }
@@ -1249,7 +1253,7 @@ void AnimationWidget::paintScene(gfx::Painter& painter, double secs)
             // draw the resize indicator. (lower right corner box)
             trans.Push();
                 trans.Scale(10.0f, 10.0f);
-                trans.Translate(size.x-10.0f, size.y-10.0f);
+                trans.Translate(size.x*0.5f-10.0f, size.y*0.5f-10.0f);
                 scene::Animation::DrawPacket sizing_box;
                 sizing_box.transform = trans.GetAsMatrix();
                 sizing_box.material  = green;
@@ -1261,6 +1265,7 @@ void AnimationWidget::paintScene(gfx::Painter& painter, double secs)
             // draw the rotation indicator. (upper left corner circle)
             trans.Push();
                 trans.Scale(10.0f, 10.0f);
+                trans.Translate(-size.x*0.5f, -size.y*0.5f);
                 scene::Animation::DrawPacket rotation_circle;
                 rotation_circle.transform = trans.GetAsMatrix();
                 rotation_circle.material  = green;

scene/animation.cpp
@@ -78,9 +78,7 @@ glm::mat4 AnimationNode::GetNodeTransform() const
     // transformation order is the order they're
     // written here.
     gfx::Transform transform;
-    // offset the object so that the center of the shape is aligned
-    // with the position parameter.
-    transform.Translate(-mSize.x * 0.5f, -mSize.y * 0.5f);
+    // transform.Scale(mScale); // currently not supported.
     transform.Rotate(mRotation);
     transform.Translate(mPosition);
     return transform.GetAsMatrix();
@@ -90,6 +88,9 @@ glm::mat4 AnimationNode::GetModelTransform() const
 {
     gfx::Transform transform;
     transform.Scale(mSize);
+    // offset the object so that the center of the shape is aligned
+    // with the position parameter.
+    transform.Translate(-mSize.x * 0.5f, -mSize.y * 0.5f);
     return transform.GetAsMatrix();
 }
 
@@ -428,20 +429,25 @@ AnimationNode* Animation::CoarseHitTest(float x, float y, glm::vec2* hitbox_pos)
                 return;
 
             mTransform.Push(node->GetNodeTransform());
+            // using the model transform will put the coordinates in the drawable coordinate
+            // space, i.e. normalized coordinates.
+            mTransform.Push(node->GetModelTransform());
 
             const auto& animation_to_node = glm::inverse(mTransform.GetAsMatrix());
             const auto& hitpoint_in_node = animation_to_node * mHitPoint;
-            const auto& size = node->GetSize();
+
             if (hitpoint_in_node.x >= 0.0f &&
-                hitpoint_in_node.x < size.x &&
+                hitpoint_in_node.x < 1.0f &&
                 hitpoint_in_node.y >= 0.0f &&
-                hitpoint_in_node.y < size.y)
+                hitpoint_in_node.y < 1.0f)
             {
+                const auto& size = node->GetSize();
                 Hit hit;
                 hit.node = node;
-                hit.point = glm::vec2(hitpoint_in_node.x, hitpoint_in_node.y);
+                hit.point = glm::vec2(hitpoint_in_node.x * size.x, hitpoint_in_node.y * size.y);
                 mHitNodes.push_back(hit);
             }
+            mTransform.Pop();
         }
         virtual void LeaveNode(AnimationNode* node) override
         {

scene/animation.h
@@ -153,7 +153,7 @@ namespace scene
         glm::mat4 GetNodeTransform() const;
 
         // Get this node's transformation that applies to this
-        // nodes' drawable object(s).
+        // node's drawable object(s).
         glm::mat4 GetModelTransform() const;
 
         // Prepare this animation component for rendering
@@ -284,24 +284,29 @@ namespace scene
         { return mRenderTree; }
 
         // Perform coarse hit test to see if the given x,y point
-        // intersects with any node in the animation. The testing
-        // is coarse in the sense that it's done against the node's
-        // size box (the box that contains the node's drawable object)
-        // only instead of actually checking against the drawable shape.
+        // intersects with any node's drawable in the animation.
+        // The testing is coarse in the sense that it's done against the node's
+        // drawable shapes size box (the box that contains the node's drawable object)
+        // only instead of actually checking against the drawable shape content.
+        // I.e. if clicking on an object that has transparency the transparency is ignored
+        // when doing selection.
         // Returns nullptr if nothing was intersected otherwise returns
         // the object with greatest layer value. (Topmost). The order
         // in case of multiple objects with equal layer value is unspecified.
         // Optionally save the hit coords in node's coordinate space. (i.e. position
-        // within the hit box)
+        // within the hit box). The box coordinates are 0, 0 in the top lef corner
+        // and width, height (size of the drawable) in the bottom right corner.
         AnimationNode* CoarseHitTest(float x, float y, glm::vec2* hitbox_pos = nullptr);
-
+        // See above.
         const AnimationNode* CoarseHitTest(float x, float y, glm::vec2* hitbox_pos = nullptr) const
         {
             return const_cast<Animation*>(this)->CoarseHitTest(x, y, hitbox_pos);
         }
 
+        // Map coordinates in some AnimationNode's (see AnimationNode::GetNodeTransform) space
+        // into animation coordinate space. 
         glm::vec2 MapCoordsFromNode(float x, float y, const AnimationNode* node) const;
-
+        // Map coordinates in animation coordinate space into some AnimationNode's coordinate space.
         glm::vec2 MapCoordsToNode(float x, float y, const AnimationNode* node) const;
 
         // Reset the state of the animation to initial state.