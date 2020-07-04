ensisoft/detonator

commit 1a29eb8193d62181aa6e62f1c85e37fef5a767d8

Flip drawable shapes' texture coordinates on Y axis
Flip drawable shape's texture coordinates on Y axis. This assumes
now that in texture addressing Y = 0.0f refers to the top of the
(i.e. the first scan line) texture and 1.0f refers to the bottom
of the texture (i.e. the last scan line). This conveniently aligns
with the fact that when texture data is uploaded GL assumes the
first scanline in the memory of the image data to be the last
scanline of the image. This basically means that the normal
convention where Y = 1.0f refers to the top of the image will
sample the last scanline of data (i.e the bottom of the image)
unless either the image is flipped when uploaded or the texture
coordinate is changed.

Instead of changing the texture coordinate via device texture matrix
we can just bake this transformation directly in the drawable shapes'
vertex data and thus simplify the code by removing the need for
such a texture matrix and having to perform the matrix mult in the
shader and also simplify the logic of the code (such as the material's
texture box) that needs to compute positions within the texture.

Additionally this scheme also aligns with the way the gl_PointCoord
varies across a point when point fragments are rasterized. So nothing
special needs to be done when using gl_PointCoord either.

graphics/drawable.cpp
@@ -55,13 +55,13 @@ Geometry* Arrow::Upload(Device& device) const
         if (!geom)
         {
             const Vertex verts[] = {
-                {{0.0f, -0.25f}, {0.0f, 0.75f}},
-                {{0.0f, -0.75f}, {0.0f, 0.25f}},
-                {{0.7f, -0.75f}, {0.7f, 0.25f}},
-                {{0.7f, -1.0f}, {0.7f, 0.0f}},
+                {{0.0f, -0.25f}, {0.0f, 0.25f}},
+                {{0.0f, -0.75f}, {0.0f, 0.75f}},
+                {{0.7f, -0.75f}, {0.7f, 0.75f}},
+                {{0.7f, -1.0f}, {0.7f, 1.0f}},
                 {{1.0f, -0.5f}, {1.0f, 0.5f}},
-                {{0.7f, -0.0f}, {0.7f, 1.0f}},
-                {{0.7f, -0.25f}, {0.7f, 0.75f}},
+                {{0.7f, -0.0f}, {0.7f, 0.0f}},
+                {{0.7f, -0.25f}, {0.7f, 0.25f}},
             };
             geom = device.MakeGeometry("ArrowOutline");
             geom->Update(verts, 7);
@@ -75,17 +75,17 @@ Geometry* Arrow::Upload(Device& device) const
         {
             const Vertex verts[] = {
                 // body
-                {{0.0f, -0.25f}, {0.0f, 0.75f}},
-                {{0.0f, -0.75f}, {0.0f, 0.25f}},
-                {{0.7f, -0.25f}, {0.7f, 0.75f}},
+                {{0.0f, -0.25f}, {0.0f, 0.25f}},
+                {{0.0f, -0.75f}, {0.0f, 0.75f}},
+                {{0.7f, -0.25f}, {0.7f, 0.25f}},
                 // body
-                {{0.7f, -0.25f}, {0.7f, 0.75f}},
-                {{0.0f, -0.75f}, {0.0f, 0.25f}},
-                {{0.7f, -0.75f}, {0.7f, 0.25f}},
+                {{0.7f, -0.25f}, {0.7f, 0.25f}},
+                {{0.0f, -0.75f}, {0.0f, 0.75f}},
+                {{0.7f, -0.75f}, {0.7f, 0.75f}},
 
                 // arrow head
-                {{0.7f, -0.0f}, {0.7f, 1.0f}},
-                {{0.7f, -1.0f}, {0.7f, 0.0f}},
+                {{0.7f, -0.0f}, {0.7f, 0.0f}},
+                {{0.7f, -1.0f}, {0.7f, 1.0f}},
                 {{1.0f, -0.5f}, {1.0f, 0.5f}},
             };
             geom = device.MakeGeometry("Arrow");
@@ -100,26 +100,26 @@ Geometry* Arrow::Upload(Device& device) const
         {
             const Vertex verts[] = {
                 // body
-                {{0.0f, -0.25f}, {0.0f, 0.75f}},
-                {{0.0f, -0.75f}, {0.0f, 0.25f}},
-                {{0.0f, -0.75f}, {0.0f, 0.25f}},
-                {{0.7f, -0.25f}, {0.7f, 0.75f}},
-                {{0.7f, -0.25f}, {0.7f, 0.75f}},
-                {{0.0f, -0.25f}, {0.0f, 0.75f}},
+                {{0.0f, -0.25f}, {0.0f, 0.25f}},
+                {{0.0f, -0.75f}, {0.0f, 0.75f}},
+                {{0.0f, -0.75f}, {0.0f, 0.75f}},
+                {{0.7f, -0.25f}, {0.7f, 0.25f}},
+                {{0.7f, -0.25f}, {0.7f, 0.25f}},
+                {{0.0f, -0.25f}, {0.0f, 0.25f}},
 
                 // body
-                {{0.0f, -0.75f}, {0.0f, 0.25f}},
-                {{0.7f, -0.75f}, {0.7f, 0.25f}},
-                {{0.7f, -0.75f}, {0.7f, 0.25f}},
-                {{0.7f, -0.25f}, {0.0f, 0.75f}},
+                {{0.0f, -0.75f}, {0.0f, 0.75f}},
+                {{0.7f, -0.75f}, {0.7f, 0.75f}},
+                {{0.7f, -0.75f}, {0.7f, 0.75f}},
+                {{0.7f, -0.25f}, {0.0f, 0.25f}},
 
                 // arrow head
-                {{0.7f, -0.0f}, {0.7f, 1.0f}},
-                {{0.7f, -1.0f}, {0.7f, 0.0f}},
-                {{0.7f, -1.0f}, {0.7f, 0.0f}},
+                {{0.7f, -0.0f}, {0.7f, 0.0f}},
+                {{0.7f, -1.0f}, {0.7f, 1.0f}},
+                {{0.7f, -1.0f}, {0.7f, 1.0f}},
                 {{1.0f, -0.5f}, {1.0f, 0.5f}},
                 {{1.0f, -0.5f}, {1.0f, 0.5f}},
-                {{0.7f, -0.0f}, {0.7f, 1.0f}},
+                {{0.7f, -0.0f}, {0.7f, 0.0f}},
             };
             geom = device.MakeGeometry("ArrowWireframe");
             geom->Update(verts, 16);
@@ -188,7 +188,7 @@ Geometry* Circle::Upload(Device& device) const
                 v.aPosition.x = x + 0.5f;
                 v.aPosition.y = y - 0.5f;
                 v.aTexCoord.x = x + 0.5f;
-                v.aTexCoord.y = y + 0.5f;
+                v.aTexCoord.y = y - 0.5f;
                 vs.push_back(v);
                 vs.push_back(center);
             }
@@ -228,10 +228,10 @@ Geometry* Rectangle::Upload(Device& device) const
         if (geom == nullptr)
         {
             const Vertex verts[] = {
-                { {0.0f,  0.0f}, {0.0f, 1.0f} },
-                { {0.0f, -1.0f}, {0.0f, 0.0f} },
-                { {1.0f, -1.0f}, {1.0f, 0.0f} },
-                { {1.0f,  0.0f}, {1.0f, 1.0f} }
+                { {0.0f,  0.0f}, {0.0f, 0.0f} },
+                { {0.0f, -1.0f}, {0.0f, 1.0f} },
+                { {1.0f, -1.0f}, {1.0f, 1.0f} },
+                { {1.0f,  0.0f}, {1.0f, 0.0f} }
             };
             geom = device.MakeGeometry("RectangleOutline");
             geom->Update(verts, 4);
@@ -244,13 +244,13 @@ Geometry* Rectangle::Upload(Device& device) const
         if (geom == nullptr)
         {
             const Vertex verts[6] = {
-                { {0.0f,  0.0f}, {0.0f, 1.0f} },
-                { {0.0f, -1.0f}, {0.0f, 0.0f} },
-                { {1.0f, -1.0f}, {1.0f, 0.0f} },
+                { {0.0f,  0.0f}, {0.0f, 0.0f} },
+                { {0.0f, -1.0f}, {0.0f, 1.0f} },
+                { {1.0f, -1.0f}, {1.0f, 1.0f} },
 
-                { {0.0f,  0.0f}, {0.0f, 1.0f} },
-                { {1.0f, -1.0f}, {1.0f, 0.0f} },
-                { {1.0f,  0.0f}, {1.0f, 1.0f} }
+                { {0.0f,  0.0f}, {0.0f, 0.0f} },
+                { {1.0f, -1.0f}, {1.0f, 1.0f} },
+                { {1.0f,  0.0f}, {1.0f, 0.0f} }
             };
             geom = device.MakeGeometry("Rectangle");
             geom->Update(verts, 6);
@@ -283,9 +283,9 @@ Geometry* Triangle::Upload(Device& device) const
     if (!geom)
     {
         const Vertex verts[3] = {
-            { {0.5f,  0.0f}, {0.5f, 1.0f} },
-            { {0.0f, -1.0f}, {0.0f, 0.0f} },
-            { {1.0f, -1.0f}, {1.0f, 0.0f} }
+            { {0.5f,  0.0f}, {0.5f, 0.0f} },
+            { {0.0f, -1.0f}, {0.0f, 1.0f} },
+            { {1.0f, -1.0f}, {1.0f, 1.0f} }
         };
         geom = device.MakeGeometry("Triangle");
         geom->Update(verts, 3);
@@ -329,18 +329,18 @@ Geometry* Grid::Upload(Device& device) const
         {
             const float x = i * xadvance;
             const Vertex line[2] = {
-                {{x,  0.0f}, {x, 1.0f}},
-                {{x, -1.0f}, {x, 0.0f}}
+                {{x,  0.0f}, {x, 0.0f}},
+                {{x, -1.0f}, {x, 1.0f}}
             };
             verts.push_back(line[0]);
             verts.push_back(line[1]);
         }
         for (unsigned i=1; i<=mNumHorizontalLines; ++i)
         {
-            const float y = i * yadvance * -1.0f;
+            const float y = i * yadvance;
             const Vertex line[2] = {
-                {{0.0f, y}, {0.0f, 1.0f + y}},
-                {{1.0f, y}, {1.0f, 1.0f + y}},
+                {{0.0f, y*-1.0f}, {0.0f, y}},
+                {{1.0f, y*-1.0f}, {1.0f, y}},
             };
             verts.push_back(line[0]);
             verts.push_back(line[1]);
@@ -349,14 +349,14 @@ Geometry* Grid::Upload(Device& device) const
         {
             const Vertex corners[4] = {
                 // top left
-                {{0.0f, 0.0f}, {0.0f, 1.0f}},
+                {{0.0f, 0.0f}, {0.0f, 0.0f}},
                 // top right
-                {{1.0f, 0.0f}, {1.0f, 1.0f}},
+                {{1.0f, 0.0f}, {1.0f, 0.0f}},
 
                 // bottom left
-                {{0.0f, -1.0f}, {0.0f, 0.0f}},
+                {{0.0f, -1.0f}, {0.0f, 1.0f}},
                 // bottom right
-                {{1.0f, -1.0f}, {1.0f, 0.0f}}
+                {{1.0f, -1.0f}, {1.0f, 1.0f}}
             };
             verts.push_back(corners[0]);
             verts.push_back(corners[1]);
@@ -402,6 +402,8 @@ Geometry* KinematicsParticleEngine::Upload(Device& device) const
         Vertex v;
         v.aPosition.x = p.position.x / mParams.max_xpos;
         v.aPosition.y = p.position.y / mParams.max_ypos * -1.0f;
+        // abusing texcoord here to pass per particle point size
+        // to the fragment shader.
         v.aTexCoord.x = p.pointsize >= 0.0f ? p.pointsize : 0.0f;
         verts.push_back(v);
     }

graphics/material.h
@@ -344,7 +344,7 @@ namespace gfx
                         ? sampler.box : sampler.box.Normalize(FSize(w, h));
 
                     const float x = box.GetX();
-                    const float y = 1.0f - box.GetY() - box.GetHeight();
+                    const float y = box.GetY();
                     const float sx = box.GetWidth();
                     const float sy = box.GetHeight();
 

graphics/opengl_es2_device.cpp
@@ -949,11 +949,18 @@ private:
             // is given to glTexImage2D doesn't match the "typical" layout
             // that is used by many toolkits/libraries. The order of scan lines
             // is reversed and glTexImage expects the first scanline (in memory)
-            // to be the bottom scanline of the image.
-            // There are two ways to deal with this dilemma:
+            // to be the bottom scanline of the image. 
+            // There are several ways to deal with this dilemma:
             // - flip all images on the horizontal axis before calling glTexImage2D.
             // - use a matrix to transform the texture coordinates to counter this.
+            // - flip texture coordinates and assume that Y=0.0f means the top of 
+            //   the texture (first scan row) and Y=1.0f means the bottom of the
+            //   the texture (last scan row).
             //
+            //
+            // this comment and the below kDeviceTextureMatrix is here only for posterity.
+            // Currently we have flipped the texture coordinates like explained above.
+            // 
             // If the program being used to render stuff is using a texture
             // we helpfully setup here a "device texture matrix" that will be provided
             // for the program and should be used to transform the texture coordinates
@@ -965,12 +972,15 @@ private:
             // It should also be possible to use the device texture matrix for example
             // in cases where the device would bake some often used textures into an atlas
             // and implicitly alter the texture coordinates.
+            //
+            /* no longer used.
             static const float kDeviceTextureMatrix[3][3] = {
                 {1.0f, 0.0f, 0.0f},
                 {0.0f, -1.0f, 0.0f},
                 {0.0f, 1.0f, 0.0f}
             };
             SetUniform("kDeviceTextureMatrix", kDeviceTextureMatrix);
+            */
 
             // keep track of textures being used so that if/when this
             // program is actually used to draw stuff we can realize

graphics/shaders/es2/texture_map.glsl
@@ -42,22 +42,18 @@ uniform vec4 kBaseColor;
 uniform mat3 kDeviceTextureMatrix;
 varying vec2 vTexCoord;
 
-vec2 SamplePointCoord()
-{
-    // "However, the gl_PointCoord fragment shader input defines
-    // a per-fragment coordinate space (s, t) where s varies from
-    // 0 to 1 across the point horizontally left-to-right, and t
-    // ranges from 0 to 1 across the point vertically top-to-bottom."
-    return vec2(gl_PointCoord.x, 1.0-gl_PointCoord.y);
-}
-
 void main()
 {
     // for texture coords we need either the coords from the
     // vertex data or gl_PointCoord if the geometry is being
     // rasterized as points.
     // we set kRenderPoints to 1.0f when rendering points.
-    vec2 coords = mix(vTexCoord, SamplePointCoord(), kRenderPoints);
+    // note about gl_PointCoord:
+    // "However, the gl_PointCoord fragment shader input defines
+    // a per-fragment coordinate space (s, t) where s varies from
+    // 0 to 1 across the point horizontally left-to-right, and t
+    // ranges from 0 to 1 across the point vertically top-to-bottom."
+    vec2 coords = mix(vTexCoord, gl_PointCoord, kRenderPoints);
     coords = coords * kTextureScale;
 
     // apply texture box transformation.
@@ -66,14 +62,10 @@ void main()
     vec2 trans_tex0 = kTextureBox0.xy;
     vec2 trans_tex1 = kTextureBox1.xy;
 
+    // scale and transform based on texture box. (todo: maybe use texture matrix?)
     vec2 c1 = coords * scale_tex0 + trans_tex0;
     vec2 c2 = coords * scale_tex1 + trans_tex1;
 
-    // apply device specific texture transformation.
-    vec3 c1_transformed = kDeviceTextureMatrix * vec3(c1.xy, 1.0);
-    vec3 c2_transformed = kDeviceTextureMatrix * vec3(c2.xy, 1.0);
-    c1 = c1_transformed.xy;
-    c2 = c2_transformed.xy;
 
     // sample textures, if texture is a just an alpha mask we use
     // only the alpha channel later.

commit f4c1aa234dcff9f93e84d3860557c4f03c41e906

Fix texture Y coordinate bug in cirle drawable
Fix texture Y coordinate bug in circle drawable. This was broken
by commit 1a29eb8193d62181aa6e62f1c85e37fef5a767d8 to flip the
texture Y coordinates.

graphics/drawable.cpp
@@ -172,13 +172,14 @@ Geometry* Circle::Upload(Device& device) const
         {
             const auto x = std::cos(angle) * 0.5f;
             const auto y = std::sin(angle) * 0.5f;
-            angle += angle_increment;
             Vertex v;
             v.aPosition.x = x + 0.5f;
             v.aPosition.y = y - 0.5f;
             v.aTexCoord.x = x + 0.5f;
-            v.aTexCoord.y = y + 0.5f;
+            v.aTexCoord.y = 1.0 - (y + 0.5f);
             vs.push_back(v);
+
+            angle += angle_increment;
 
             if (mStyle == Style::Wireframe)
             {
@@ -188,7 +189,7 @@ Geometry* Circle::Upload(Device& device) const
                 v.aPosition.x = x + 0.5f;
                 v.aPosition.y = y - 0.5f;
                 v.aTexCoord.x = x + 0.5f;
-                v.aTexCoord.y = y - 0.5f;
+                v.aTexCoord.y = 1.0 - (y + 0.5f);
                 vs.push_back(v);
                 vs.push_back(center);
             }