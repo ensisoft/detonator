ensisoft/detonator

commit 0f0b1d45c98d146c9011a70a2b4ac989fa05395c

Refactor TextBuffer text and styling setting and add JSON support
Refactor TextBuffer text and styling setting and add JSON support.
This change removes the TextStyle type and combines the styling
information with the Text type structure which is now public in the
class API in order to support easy read access to the current text
buffer text content.

Additionally add support for JSON based serialization.

pinyin-invaders/gamewidget.cpp
@@ -862,9 +862,12 @@ public:
 
         const auto fontsize = unitScale.GetY() / 1.75;
         gfx::TextBuffer text(shipScaledWidth, shipScaledHeight);
-        text.AddText(base::ToUtf8(mText),"fonts/SourceHanSerifTC-SemiBold.otf", fontsize)
-            .SetAlign(gfx::TextBuffer::HorizontalAlignment::AlignLeft)
-            .SetAlign(gfx::TextBuffer::VerticalAlignment::AlignCenter);
+        gfx::TextBuffer::Text text_and_style;
+        text_and_style.font = "fonts/SourceHanSerifTC-SemiBold.otf";
+        text_and_style.fontsize = fontsize;
+        text_and_style.halign = gfx::TextBuffer::HorizontalAlignment::AlignLeft;
+        text_and_style.valign = gfx::TextBuffer::VerticalAlignment::AlignCenter;
+        text.AddText(std::move(text_and_style));
 
         t.Translate(shipScaledWidth * 0.6 + jetScaledWidth * 0.75, 0);
 

editor/gui/dlgtext.cpp
@@ -85,14 +85,15 @@ void DlgText::PaintScene(gfx::Painter& painter, double secs)
     const unsigned buffer_height = GetValue(mUI.bufferHeight);
     gfx::TextBuffer text_buffer(buffer_width, buffer_height);
 
-    gfx::TextBuffer::TextStyle style;
-    style.SetFontsize(GetValue(mUI.fontSize));
-    style.SetUnderline(GetValue(mUI.underline));
-    style.SetAlign((gfx::TextBuffer::VerticalAlignment)GetValue(mUI.cmbVAlign));
-    style.SetAlign((gfx::TextBuffer::HorizontalAlignment)GetValue(mUI.cmbHAlign));
-    style.SetLineHeight(GetValue(mUI.lineHeight));
-
-    text_buffer.AddText(app::ToUtf8(text), app::ToUtf8(font_file), style);
+    gfx::TextBuffer::Text text_and_style;
+    text_and_style.text      = app::ToUtf8(text);
+    text_and_style.font      = app::ToUtf8(font_file);
+    text_and_style.fontsize  = GetValue(mUI.fontSize);
+    text_and_style.underline = GetValue(mUI.underline);
+    text_and_style.valign    = GetValue(mUI.cmbVAlign);
+    text_and_style.halign    = GetValue(mUI.cmbHAlign);
+    text_and_style.lineheight = GetValue(mUI.lineHeight);
+    text_buffer.AddText(std::move(text_and_style));
 
     gfx::Material material;
     material.SetType(gfx::Material::Type::Texture);

graphics/drawing.cpp
@@ -64,8 +64,14 @@ void DrawTextRect(Painter& painter,
     const bool underline = properties & TextProp::Underline;
     const bool blinking  = properties & TextProp::Blinking;
 
-    buff.AddText(text, font, font_size_px)
-        .SetAlign(va).SetAlign(ha).SetUnderline(underline);
+    TextBuffer::Text text_and_style;
+    text_and_style.text = text;
+    text_and_style.font = font;
+    text_and_style.fontsize = font_size_px;
+    text_and_style.underline = underline;
+    text_and_style.halign = ha;
+    text_and_style.valign = va;
+    buff.AddText(text_and_style);
 
     auto material = BitmapText(buff);
     material.SetBaseColor(color);

graphics/text.cpp
@@ -124,7 +124,7 @@ std::shared_ptr<Bitmap<Grayscale>> TextBuffer::Rasterize() const
             if (line.empty())
                 line = "k";
 
-            auto bmp = Rasterize(line, text.font, text.style);
+            auto bmp = Rasterize(line, text);
             if (was_empty)
                 bmp->Fill(Grayscale{0});
 
@@ -132,21 +132,21 @@ std::shared_ptr<Bitmap<Grayscale>> TextBuffer::Rasterize() const
             line_bitmaps.push_back(std::move(bmp));
         }
         int ypos = 0;
-        if (text.style.mVAlignment == VerticalAlignment::AlignTop)
+        if (text.valign == VerticalAlignment::AlignTop)
             ypos = 0;
-        else if (text.style.mVAlignment == VerticalAlignment::AlignCenter)
+        else if (text.valign == VerticalAlignment::AlignCenter)
             ypos = ((int)mHeight - total_height) / 2;
-        else if (text.style.mVAlignment == VerticalAlignment::AlignBottom)
+        else if (text.valign == VerticalAlignment::AlignBottom)
             ypos = mHeight - total_height;
 
         for (const auto& bmp : line_bitmaps)
         {
             int xpos = 0;
-            if (text.style.mHAlignment == HorizontalAlignment::AlignLeft)
+            if (text.halign == HorizontalAlignment::AlignLeft)
                 xpos = 0;
-            else if (text.style.mHAlignment == HorizontalAlignment::AlignCenter)
+            else if (text.halign == HorizontalAlignment::AlignCenter)
                 xpos = ((int)mWidth - (int)bmp->GetWidth()) / 2;
-            else if (text.style.mHAlignment == HorizontalAlignment::AlignRight)
+            else if (text.halign == HorizontalAlignment::AlignRight)
                 xpos = mWidth - bmp->GetWidth();
 
             out->Copy(xpos, ypos, *bmp);
@@ -165,26 +165,6 @@ std::shared_ptr<Bitmap<Grayscale>> TextBuffer::Rasterize() const
     return out;
 }
 
-TextBuffer::TextStyle& TextBuffer::AddText(const std::string& text, const std::string& font, unsigned font_size_px)
-{
-    Text t;
-    t.text = text;
-    t.font = font;
-    t.style.SetFontsize(font_size_px);
-
-    mText.push_back(t);
-    return mText.back().style;
-}
-
-void TextBuffer::AddText(const std::string& text, const std::string& font, const TextStyle& style)
-{
-    Text t;
-    t.text = text;
-    t.font = font;
-    t.style = style;
-    mText.push_back(t);
-}
-
 std::size_t TextBuffer::GetHash() const
 {
     std::size_t hash = 0;
@@ -195,17 +175,64 @@ std::size_t TextBuffer::GetHash() const
         // have the style properties also contribute to the hash so that
         // text buffer with similar text content but different properties
         // generates a different hash.
-        hash = base::hash_combine(hash, (unsigned)t.style.mLineHeight);
-        hash = base::hash_combine(hash, (unsigned)t.style.mFontsize);
-        hash = base::hash_combine(hash, (unsigned)t.style.mHAlignment);
-        hash = base::hash_combine(hash, (unsigned)t.style.mVAlignment);
-        hash = base::hash_combine(hash, (unsigned)t.style.mUnderline);
+        hash = base::hash_combine(hash, (unsigned)t.lineheight);
+        hash = base::hash_combine(hash, (unsigned)t.fontsize);
+        hash = base::hash_combine(hash, (unsigned)t.halign);
+        hash = base::hash_combine(hash, (unsigned)t.valign);
+        hash = base::hash_combine(hash, (unsigned)t.underline);
     }
     return hash;
 }
 
-std::shared_ptr<Bitmap<Grayscale>> TextBuffer::Rasterize(const std::string& text,
-    const std::string& font, const TextStyle& style) const
+nlohmann::json TextBuffer::ToJson() const
+{
+    nlohmann::json json;
+    base::JsonWrite(json, "width", mWidth);
+    base::JsonWrite(json, "height", mHeight);
+    for (const auto& text : mText)
+    {
+        nlohmann::json js;
+        base::JsonWrite(js, "string", text.text);
+        base::JsonWrite(js, "font_file", text.font);
+        base::JsonWrite(js, "font_size", text.fontsize);
+        base::JsonWrite(js, "line_height", text.lineheight);
+        base::JsonWrite(js, "underline", text.underline);
+        base::JsonWrite(js, "horizontal_alignment", text.halign);
+        base::JsonWrite(js, "vertical_alignment", text.valign);
+        json["texts"].push_back(js);
+    }
+    return json;
+}
+
+// static
+std::optional<TextBuffer> TextBuffer::FromJson(const nlohmann::json& json)
+{
+    TextBuffer buffer;
+    if (!base::JsonReadSafe(json, "width", &buffer.mWidth) ||
+        !base::JsonReadSafe(json, "height", &buffer.mHeight))
+        return std::nullopt;
+
+    if (!json.contains("texts"))
+        return buffer;
+
+    for (const auto& json_text : json["texts"].items())
+    {
+        const auto& js = json_text.value();
+        Text t;
+        if (!base::JsonReadSafe(js, "string", &t.text) ||
+            !base::JsonReadSafe(js, "font_file", &t.font) ||
+            !base::JsonReadSafe(js, "font_size", &t.fontsize) ||
+            !base::JsonReadSafe(js, "line_height", &t.lineheight) |
+            !base::JsonReadSafe(js, "underline", &t.underline) ||
+            !base::JsonReadSafe(js, "horizontal_alignment", &t.halign) ||
+            !base::JsonReadSafe(js, "vertical_alignment", &t.valign))
+            return std::nullopt;
+        buffer.mText.push_back(std::move(t));
+    }
+    return buffer;
+}
+
+std::shared_ptr<Bitmap<Grayscale>> TextBuffer::Rasterize(const std::string& line, const Text& text) const
 {
     // FreeType 2 uses size objects to model all information related to a given character
     // size for a given face. For example, a size object holds the value of certain metrics
@@ -222,15 +249,15 @@ std::shared_ptr<Bitmap<Grayscale>> TextBuffer::Rasterize(const std::string& text
     }
     FT_Face face;
 
-    if (FT_New_Face(mFreetype->library, font.c_str(), 0, &face))
-        throw std::runtime_error("Failed to load font file: " + font);
+    if (FT_New_Face(mFreetype->library, text.font.c_str(), 0, &face))
+        throw std::runtime_error("Failed to load font file: " + text.font);
     auto face_raii = base::MakeUniqueHandle(face, FT_Done_Face);
 
     if (FT_Select_Charmap(face, FT_ENCODING_UNICODE))
         throw std::runtime_error("Font doesn't support Unicode");
 
-    if (FT_Set_Pixel_Sizes(face, 0, style.mFontsize))
-        throw std::runtime_error("Font doesn't support pixel size: " + std::to_string(style.mFontsize));
+    if (FT_Set_Pixel_Sizes(face, 0, text.fontsize))
+        throw std::runtime_error("Font doesn't support pixel size: " + std::to_string(text.fontsize));
     //if (FT_Set_Char_Size(face, font_size * FUCKING_MAGIC_SCALE, font_size * FUCKING_MAGIC_SCALE, 0, 0))
     //    throw std::runtime_error("Font doesn't support pixel size: " + std::to_string(font_size));
 
@@ -238,7 +265,7 @@ std::shared_ptr<Bitmap<Grayscale>> TextBuffer::Rasterize(const std::string& text
     // https://github.com/harfbuzz/harfbuzz-tutorial/blob/master/hello-harfbuzz-freetype.c
     hb_font_t* hb_font = hb_ft_font_create(face, nullptr);
     hb_buffer_t* hb_buff = hb_buffer_create();
-    hb_buffer_add_utf8(hb_buff, text.c_str(),
+    hb_buffer_add_utf8(hb_buff, line.c_str(),
         -1,  // NUL terminated string
          0, // offset of the first character to add to the buffer
         -1 // number of characters to add to the buffer or -1 for "until the end of text"
@@ -345,7 +372,7 @@ std::shared_ptr<Bitmap<Grayscale>> TextBuffer::Rasterize(const std::string& text
     const auto underline_position  = face->underline_position / FUCKING_MAGIC_SCALE;
     // vertical thickness of the underline.. units ??
     const auto underline_thickness = 2; // face->underline_thickness ? face->underline_thickness : 1;
-    const auto line_spacing = (face->size->metrics.height / FUCKING_MAGIC_SCALE) * style.mLineHeight;
+    const auto line_spacing = (face->size->metrics.height / FUCKING_MAGIC_SCALE) * text.lineheight;
     const auto margin = line_spacing > height ? line_spacing - height : 0;
 
     height += margin;
@@ -406,7 +433,7 @@ std::shared_ptr<Bitmap<Grayscale>> TextBuffer::Rasterize(const std::string& text
         pen_y += ya;
     }
 
-    if (style.mUnderline)
+    if (text.underline)
     {
         const auto width = bmp->GetWidth();
         const URect underline(0, baseline + underline_position,

graphics/text.h
@@ -24,9 +24,14 @@
 
 #include "config.h"
 
+#include "warnpush.h"
+#  include <nlohmann/json.hpp>
+#include "warnpop.h"
+
 #include <string>
 #include <vector>
 #include <memory>
+#include <optional>
 
 #include "base/assert.h"
 #include "bitmap.h"
@@ -82,86 +87,113 @@ namespace gfx
             AlignBottom
         };
 
-        // TextStyle object can be used to set some text styling properties
-        // such as the line height scaling factor, underline and text
-        // alignment.
         // Some general notes about text styling:
         // Common "styling" options such as Italic and Bold text are normally
         // variations of the "Regular" font. Therefore this API provides no
         // facilities for dealing with "bold" or "italic" text. Simply use
         // the appropriate font file when adding text to the TextBuffer.
-        class TextStyle
-        {
-        public:
-            // Set underlining on/off.
-            TextStyle& SetUnderline(bool underline)
-            {
-                mUnderline = underline;
-                return *this;
-            }
-            // Set text horizontal alignment wrt TextBuffer bounds.
-            TextStyle& SetAlign(HorizontalAlignment align)
-            {
-                mHAlignment = align;
-                return *this;
-            }
-            // Set text vertical alignment wrt TextBuffer bounds.
-            TextStyle& SetAlign(VerticalAlignment align)
-            {
-                mVAlignment = align;
-                return *this;
-            }
-            // Set text font size.
-            TextStyle& SetFontsize(unsigned fontsize)
-            {
-                mFontsize = fontsize;
-                return *this;
-            }
-            // Set the line height scaling factor. Default is 1.0
-            // which uses the line height from the font unscaled.
-            TextStyle& SetLineHeight(float height)
-            {
-                mLineHeight = height;
-                return *this;
-            }
-        private:
-            float mLineHeight = 1.0f;
-            bool mUnderline = false;
-            unsigned mFontsize = 0;
-            HorizontalAlignment mHAlignment = HorizontalAlignment::AlignCenter;
-            VerticalAlignment mVAlignment = VerticalAlignment::AlignCenter;
-            friend class TextBuffer;
+        // block of text that is to be rasterized in the buffer.
+
+        // A block of text with some particular formatting and font setting.
+        // Note that the text may contain new lines in which case the content
+        // (the text string) is split to multiple lines.
+        struct Text {
+            // The actual text string. UTF-8 encoded.
+            std::string text;
+
+            // The font that will be used.
+            // This is a filepath to some particular font file.
+            std::string font;
+            // The style options that apply to the text;
+
+            // Font size (in pixels)
+            unsigned fontsize = 0;
+
+            // the lineheight multiplier that is used to compute the
+            // actual text lineheight which is used to advance
+            // from one line to line in the buffer when rasterizing
+            // lines of text
+            float lineheight = 1.0f;
+
+            // Text underline flag.
+            bool underline = false;
+
+            // horizontal text alignment with respect to the rasterized buffer.
+            HorizontalAlignment halign = HorizontalAlignment::AlignCenter;
+
+            // vertical text alignment with respect to the rasterized buffer.
+            VerticalAlignment valign = VerticalAlignment::AlignCenter;
         };
 
-        // Add text to the buffer and position automatically wrt TextBuffer bounds.
-        // Returns a TextStyle object which can be used to set individual
-        // text style properties.
-        TextStyle& AddText(const std::string& text, const std::string& font, unsigned font_size_px);
+        // Add text to the buffer.
+        void AddText(const std::string& text, const std::string& font, unsigned font_size_px)
+        {
+            Text t;
+            t.text = text;
+            t.font = font;
+            t.fontsize = font_size_px;
+            mText.push_back(t);
+        }
 
-        void AddText(const std::string& text, const std::string& font, const TextStyle& style);
+        // Add text to the buffer for rasterization.
+        void AddText(const Text& text)
+        {
+            mText.push_back(text);
+        }
+
+        // add text to the buffer for rasterization.
+        void AddText(Text&& text)
+        {
+            mText.push_back(std::move(text));
+        }
+
+        // Clear all texts from the text buffer.
+        void ClearText()
+        {
+            mText.clear();
+        }
+
+        // Get the number of text objects currently in the text buffer.
+        size_t GetNumTexts()
+        { return mText.size(); }
+
+        // Get the text blob at the given index
+        const Text& GetText(size_t index) const
+        {
+            ASSERT(index < mText.size());
+            return mText[index];
+        }
+        // Get the text blob at the given index.
+        Text& GetText(size_t index)
+        {
+            ASSERT(index < mText.size());
+            return mText[index];
+        }
+        // Returns true if the text buffer contains now text objects
+        // otherwise false.
+        bool IsEmpty() const
+        { return mText.empty(); }
 
         // Compute hash of the contents of the string buffer.
         std::size_t GetHash() const;
+
+        // Serialize the contents into JSON
+        nlohmann::json ToJson() const;
+
+        // Load a TextBuffer from JSON.
+        static std::optional<TextBuffer> FromJson(const nlohmann::json& json);
 
     private:
         struct FontLibrary;
         mutable std::shared_ptr<FontLibrary> mFreetype;
         static std::weak_ptr<FontLibrary> Freetype;
-        struct Text;
-        std::shared_ptr<Bitmap<Grayscale>> Rasterize(
-            const std::string& text,
-            const std::string& font,
-            const TextStyle& style) const;
+
+        std::shared_ptr<Bitmap<Grayscale>> Rasterize(const std::string& text, const Text& style) const;
 
     private:
         unsigned mWidth  = 0;
         unsigned mHeight = 0;
 
-        struct Text {
-            std::string text;
-            std::string font;
-            TextStyle style;
-        };
         std::vector<Text> mText;
     };
 

commit a93ba216dff091499afaaac98f6ace7fe729b6c5

Fix setting the actual renderable text string in gamewidget
Fix setting the actual renderable text string in gamewidget. This
bug was introduced in commit 0f0b1d45c98d146c9011a70a2b4ac989fa05395c

pinyin-invaders/gamewidget.cpp
@@ -863,6 +863,7 @@ public:
         const auto fontsize = unitScale.GetY() / 1.75;
         gfx::TextBuffer text(shipScaledWidth, shipScaledHeight);
         gfx::TextBuffer::Text text_and_style;
+        text_and_style.text = base::ToUtf8(mText);
         text_and_style.font = "fonts/SourceHanSerifTC-SemiBold.otf";
         text_and_style.fontsize = fontsize;
         text_and_style.halign = gfx::TextBuffer::HorizontalAlignment::AlignLeft;