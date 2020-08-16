202ddf9fcbbd7bcb8e5d39cdcc6755a42fff64e0
Sami Väisänen <samiv@ensisoft.com>
2020-08-16 13:39:22 +0300

    Add simplistic command line argument parser
    Add simplistic command line argument parser. Currently this cmdline
    argument parsing system provides no functionality for semantic
    validation of arguments but this is left for the client. (i.e.
    to check whether all required arguments were given or whether
    mutually exlusive args are in conflict).

---
+++ base/cmdline.h
@@ -0,0 +1,365 @@
+// Copyright (c) 2016 Sami Väisänen, Ensisoft
+//
+// http://www.ensisoft.com
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
+
+#include "platform.h"
+
+#pragma once
+
+#include <string>
+#include <typeinfo>
+#include <stdexcept>
+#include <memory>
+#include <vector>
+#include <algorithm>
+#include <sstream>
+#include <iomanip>
+#include <cassert>
+
+// simplistic command line parser
+
+namespace base
+{
+    namespace detail {
+        template<typename T>
+        T FromString(const char* str)
+        {
+            std::stringstream ss;
+            ss << str;
+            T value;
+            ss >> value;
+            if (ss.fail())
+                throw std::runtime_error(std::string("Can't interpret '") +  str + "' as wanted value type.");
+            return value;
+        }
+    } // detail
+
+    // CommandLineArgumentStack initialized from a set of arguments given as a
+    // an array of char* pointers and length of array.
+    class CommandLineArgumentStack
+    {
+    public:
+        // Initialize the command line from the given char* array
+        // and the legth of the array.  This will copy the data.
+        // so it's safe to discard the array after the constructor
+        // returns.
+        CommandLineArgumentStack(int count, const char* argv[])
+        {
+            for (int i=0; i<count; ++i)
+            {
+                std::string tmp(argv[i]);
+                const auto pos = tmp.find_first_of('=');
+                if (pos == std::string::npos)
+                {
+                    m_argv.push_back(std::move(tmp));
+                    continue;
+                }
+                auto key = tmp.substr(0, pos);
+                auto val = tmp.substr(pos+1);
+                m_argv.push_back(std::move(key));
+                if (!val.empty())
+                    m_argv.push_back(std::move(val));
+            }
+        }
+        // Get the current argument.
+        const char* GetCurrent() const
+        { return m_argv[m_pos].c_str(); }
+        // Returns true if there are still arguments available.
+        bool HasNext() const
+        { return m_pos < m_argv.size(); }
+        // Pop the current argument.
+        void Pop()
+        { ++m_pos; }
+        // Returns true if the current argument matches the given name.
+        bool IsMatch(const std::string& name) const
+        { return name == GetCurrent(); }
+    private:
+        std::size_t m_pos = 0;
+        std::vector<std::string> m_argv;
+    };
+
+    // Interface for argument processing.
+    class CommandLineArg
+    {
+    public:
+        virtual ~CommandLineArg() = default;
+        // Try to accept the current argument and consume the expected
+        // number of associated values from the command line stack.
+        // Returns true if succesful or false if the command line stack
+        // didn't contain the expected data.
+        virtual bool Accept(CommandLineArgumentStack& cmd) = 0;
+        // Checks whether this argument object matches the given name.
+        virtual bool IsMatch(const std::string& name) const = 0;
+        // Returns true if the argument was matched when the command line
+        // args were parsed. Returns false which means that the arguments data
+        // didn't contain anything for this particular arg.
+        virtual bool WasMatched() const = 0;
+        // Get the argument's name as it it's to be given on the command line.
+        virtual const std::string& GetName() const = 0;
+        // Get the user readable help string associated with this argument.
+        virtual const std::string& GetHelp() const = 0;
+
+        // Get the value of the argument converted into some type.
+        template<typename T>
+        operator const T& ()const
+        {
+            const void* ptr = this->retrieve(typeid(T));
+            assert(ptr != nullptr && "Wrong type for command line argument.");
+            return *static_cast<const T*>(ptr);
+        }
+
+        // Get the value of the argument converted into some type.
+        template<typename T>
+        const T& As() const
+        {
+            const void* ptr = this->retrieve(typeid(T));
+            assert(ptr != nullptr && "Wrong type for command line argument.");
+            return *static_cast<const T*>(ptr);
+        }
+
+    protected:
+        virtual const void* retrieve(const std::type_info& matching_type) const = 0;
+
+    private:
+    };
+
+    // Command line (on/off) flag. I.e. something doesn't expect any value.
+    // For example --enable-foo
+    class CommandLineFlag : public CommandLineArg
+    {
+    public:
+        explicit
+        CommandLineFlag(const std::string& flag_name, const std::string& flag_help)
+        : m_name(flag_name), m_help(flag_help), m_value(false)
+        {}
+
+        virtual bool Accept(CommandLineArgumentStack& cmd) override
+        {
+            if (!cmd.IsMatch(m_name))
+                return false;
+
+            cmd.Pop();
+            m_value = true;
+            m_match = true;
+            return true;
+        }
+        virtual bool IsMatch(const std::string& name) const override
+        { return name == m_name; }
+        virtual bool WasMatched() const override
+        { return m_match; }
+        virtual const std::string& GetName() const override
+        { return m_name; }
+        virtual const std::string& GetHelp() const override
+        { return m_help; }
+
+        // Returns true if the flag is set after the arguments have been parsed.
+        bool IsSet() const
+        { return m_value; }
+
+    protected:
+        virtual const void* retrieve(const std::type_info& matching_type) const override
+        {
+            if (typeid(m_value) == matching_type)
+                return &m_value;
+            return nullptr;
+        }
+
+    private:
+        std::string m_name;
+        std::string m_help;
+        bool m_value   = false;
+        bool m_match   = false;
+    };
+
+    // "--something value" kind of argument
+    template<typename T>
+    class CommandLineValue : public CommandLineArg
+    {
+    public:
+        explicit
+        CommandLineValue(const std::string& name, const std::string& help, const T& initial)
+        : m_name(name), m_help(help), m_value(initial)
+        {}
+
+        virtual bool Accept(CommandLineArgumentStack& cmd) override
+        {
+            if (!cmd.IsMatch(m_name))
+                return false;
+
+            cmd.Pop();
+            // Returns true if there's still the next argument
+            if (!cmd.HasNext())
+                throw std::runtime_error("Missing value for argument: '" + m_name + "'");
+            m_value = detail::FromString<T>(cmd.GetCurrent());
+            m_match = true;
+            cmd.Pop();
+            return true;
+        }
+        virtual bool IsMatch(const std::string& name) const override
+        { return name == m_name; }
+        virtual bool WasMatched() const override
+        { return m_match; }
+        virtual const std::string& GetName() const override
+        { return m_name; }
+        virtual const std::string& GetHelp() const override
+        { return m_help; }
+
+    protected:
+        virtual const void* retrieve(const std::type_info& matching_type) const override
+        {
+            if (typeid(m_value) == matching_type)
+                return &m_value;
+            return nullptr;
+        }
+
+    private:
+        std::string m_name;
+        std::string m_help;
+        T m_value;
+        bool m_match = false;
+    };
+
+    // CommandLineOptions provides the main interface for parsing
+    // a set of command line arguments. This is a two step process.
+    // First the command line options instance is created and configured
+    // with the appropriate command line args that are expected and
+    // matched agains the actual command line args given by the user.
+    class CommandLineOptions
+    {
+    public:
+        void Add(std::unique_ptr<CommandLineArg> arg)
+        { m_options_list.push_back(std::move(arg)); }
+
+        // Add a new command line value to be recognized.
+        template<typename T>
+        void Add(const std::string& name, const std::string& help, T initial_value)
+        {
+            Add(std::make_unique<CommandLineValue<T>>(name, help, initial_value));
+        }
+
+        // Add a new command line flag to be recognized.
+        void Add(const std::string& name, const std::string& help)
+        {
+            Add(std::make_unique<CommandLineFlag>(name, help));
+        }
+
+        // Consume the data from the command line argument stack
+        // and try to match the actual command line parameters against
+        // the ones that have been configured in this  command line options object.
+        // Returns true if command line was consumed succesfully, otherwise false
+        // and some argument could not be matched properly.
+        // On unexpected input an exception is thrown.
+        void Parse(CommandLineArgumentStack& cmd, bool allow_unexpected = false)
+        {
+            while (cmd.HasNext())
+            {
+                bool was_accepted = false;
+                for (auto& opt : m_options_list)
+                {
+                    if (opt->Accept(cmd))
+                    {
+                        was_accepted = true;
+                        break;
+                    }
+                }
+                if (!was_accepted && !allow_unexpected)
+                    throw std::logic_error(std::string("Unexpected argument: ") + cmd.GetCurrent());
+                else if (!was_accepted)
+                    cmd.Pop();
+
+            }
+        }
+
+        // Convenience wrapper for converting a parse failure into boolean
+        // result for simplicity. if the error string pointer is non nullptr
+        // then on error the error message is copied into this string.
+        bool Parse(CommandLineArgumentStack& cmd, std::string* error)
+        {
+            try
+            {
+                Parse(cmd);
+            }
+            catch (const std::exception& e)
+            {
+                if (error) *error = e.what();
+                return false;
+            }
+            return true;
+        }
+
+        const CommandLineArg& Get(const char* name) const
+        {
+            auto it = std::find_if(std::begin(m_options_list), std::end(m_options_list),
+               [name](const auto& arg) {
+                   return arg->IsMatch(name);
+               });
+            assert(it != std::end(m_options_list) && "No such argument");
+            return *(*it);
+        }
+
+        // Checks whether the argument identified by name was actually given
+        // in the list of command line arguments.
+        bool WasGiven(const char* name) const
+        {
+            for (const auto& opt : m_options_list)
+            {
+                if (opt->IsMatch(name) && opt->WasMatched())
+                    return true;
+            }
+            return false;
+        }
+
+        template<typename T>
+        T GetValue(const char* name) const
+        {
+            for (const auto& opt : m_options_list)
+            {
+                if (opt->IsMatch(name))
+                    return opt->As<T>();
+            }
+            assert("No such argument.");
+            return T();
+        }
+
+        void Print(std::ostream& out) const
+        {
+            size_t longest_switch = 0;
+            for (const auto& arg : m_options_list)
+            {
+                const auto& str = arg->GetName();
+                longest_switch  = std::max(longest_switch, str.size());
+            }
+            for (const auto& arg : m_options_list)
+            {
+                const auto& name = arg->GetName();
+                const auto& help = arg->GetHelp();
+                out << name << std::setw(longest_switch + 1 - name.size()) << std::setfill(' ') << " ";
+                out << "\t";
+                out << help;
+                out << std::endl;
+            }
+        }
+    private:
+        using list = std::vector<std::unique_ptr<CommandLineArg>>;
+        list m_options_list;
+    };
+
+} // base
---
+++ base/unit_test/unit_test_cmdline.cpp
@@ -0,0 +1,186 @@
+// Copyright (c) 2010-2020 Sami Väisänen, Ensisoft
+//
+// http://www.ensisoft.com
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
+
+#include "config.h"
+
+#include <string>
+#include <iostream>
+
+#include "base/test_minimal.h"
+#include "base/test_float.h"
+#include "base/cmdline.h"
+
+int test_main(int argc, char* argv[])
+{
+    // empty cmdline
+    {
+        const char* argv[] = {
+            "--foo",
+            "--bar"
+        };
+
+        base::CommandLineArgumentStack args(0, argv);
+        base::CommandLineOptions opt;
+        opt.Parse(args);
+    }
+
+    // on/off flags
+    {
+        const char* argv[] = {
+            "--foo",
+            "--bar"
+        };
+        base::CommandLineArgumentStack args(2, argv);
+        base::CommandLineOptions opt;
+        opt.Add("--foo", "foo help");
+        opt.Add("--bar", "bar help");
+        opt.Parse(args);
+        TEST_REQUIRE(opt.WasGiven("--foo"));
+        TEST_REQUIRE(opt.WasGiven("--bar"));
+    }
+
+    // on/off flags
+    {
+        const char* argv[] = {
+            "--foo",
+            "--bar"
+        };
+        base::CommandLineArgumentStack args(2, argv);
+        base::CommandLineOptions opt;
+        opt.Add("--foo", "foo help");
+        opt.Add("--bar", "bar help");
+        opt.Add("--keke", "keke help");
+        opt.Parse(args);
+        TEST_REQUIRE(opt.WasGiven("--foo"));
+        TEST_REQUIRE(opt.WasGiven("--bar"));
+        TEST_REQUIRE(opt.WasGiven("--keke") == false);
+    }
+
+    // with value parsing when values are given.
+    {
+        const char* argv[] = {
+            "--integer=1234",
+            "--string=foobar"
+        };
+        base::CommandLineArgumentStack args(2, argv);
+        base::CommandLineOptions opt;
+        opt.Add("--integer", "integer help", 0);
+        opt.Add("--string", "string help", std::string(""));
+        opt.Parse(args);
+        TEST_REQUIRE(opt.WasGiven("--integer"));
+        TEST_REQUIRE(opt.WasGiven("--string"));
+        TEST_REQUIRE(opt.GetValue<int>("--integer") == 1234);
+        TEST_REQUIRE(opt.GetValue<std::string>("--string") == "foobar");
+    }
+
+    // with value parsing when values are missing.
+    {
+        const char* argv[] = {};
+
+        base::CommandLineArgumentStack args(0, argv);
+        base::CommandLineOptions opt;
+        opt.Add("--integer", "integer help", 4444);
+        opt.Add("--string", "string help", std::string("default"));
+        opt.Parse(args);
+        TEST_REQUIRE(opt.WasGiven("--integer") == false);
+        TEST_REQUIRE(opt.WasGiven("--string") == false);
+        TEST_REQUIRE(opt.GetValue<int>("--integer") == 4444);
+        TEST_REQUIRE(opt.GetValue<std::string>("--string") == "default");
+    }
+
+    // float value. (special since the string format is locale specific)
+    {
+        const float test_value = 12345.0f;
+        const auto str = "--float=" + std::to_string(test_value);
+
+        const char* argv[] = {
+            str.c_str()
+        };
+        base::CommandLineArgumentStack args(1, argv);
+        base::CommandLineOptions opt;
+        opt.Add("--float", "float help", 0.0f);
+        opt.Parse(args);
+        TEST_REQUIRE(opt.WasGiven("--float"));
+        TEST_REQUIRE(real::equals(opt.GetValue<float>("--float"), test_value));
+    }
+
+    // float value. (special since the string format is locale specific)
+    {
+        const float test_value = 12345.0f;
+        const char* argv[] = {};
+
+        base::CommandLineArgumentStack args(0, argv);
+        base::CommandLineOptions opt;
+        opt.Add("--float", "float help", test_value);
+        opt.Parse(args);
+        TEST_REQUIRE(opt.WasGiven("--float") == false);
+        TEST_REQUIRE(real::equals(opt.GetValue<float>("--float"), test_value));
+    }
+
+    // test parsing/matching errors.
+
+    // unrecognized argument
+    {
+        const char* argv[] = {"--whatever"};
+        base::CommandLineArgumentStack args(1, argv);
+        base::CommandLineOptions opt;
+        std::string error;
+        TEST_REQUIRE(opt.Parse(args, &error) == false);
+        std::cout << error << "\n";
+    }
+
+    // missing value
+    {
+        const char* argv[] = {
+            "--integer=",
+        };
+        base::CommandLineArgumentStack args(1, argv);
+        base::CommandLineOptions opt;
+        opt.Add("--integer", "help", 1234);
+        std::string error;
+        TEST_REQUIRE(opt.Parse(args, &error) == false);
+        std::cout << error << "\n";
+    }
+
+    // unpexpected format error
+    {
+        const char* argv[] = {
+            "--integer=asgbasbdfas",
+        };
+        base::CommandLineArgumentStack args(1, argv);
+        base::CommandLineOptions opt;
+        opt.Add("--integer", "help", 1234);
+        std::string error;
+        TEST_REQUIRE(opt.Parse(args, &error) == false);
+        std::cout << error << "\n";
+    }
+
+    // print
+    {
+        base::CommandLineOptions opt;
+        opt.Add("--foo", "Foo help bla bla bla", 123);
+        opt.Add("--bar", "Bar help");
+        opt.Print(std::cout);
+    }
+
+    return 0;
+}
