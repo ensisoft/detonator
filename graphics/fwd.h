2cb4b93b2cef14dd75284354619fd86307a92881
Sami Väisänen <samiv@ensisoft.com>
2021-01-09 14:39:08 +0200

    Add a forward declaration header for graphics types
    add a forward declaration header for graphics types for some of
    the key graphics classes.

---
+++ graphics/fwd.h
@@ -0,0 +1,19 @@
+
+// some forward declarations for the key graphics interfaces
+
+#pragma once
+
+namespace gfx
+{
+    class Painter;
+    class Material;
+    class MaterialClass;
+    class Drawable;
+    class DrawableClass;
+    class Transform;
+    class Device;
+    class IBitmap;
+    class IBitmapGenerator;
+    class Image;
+    class Text;
+} // namespace
