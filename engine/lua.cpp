bbd671d9938b80c6b56dba54d335eb27ce6500da
Sami Väisänen <samiv@ensisoft.com>
2021-08-31 13:48:47 +0300

    Add missing glm::vec3 and glm::vec4 Lua bindings
    Add missing glm::vec3 and glm::vec4 Lua bindings.

--- engine/lua.cpp
+++ engine/lua.cpp
@@ -325,6 +325,31 @@ private:
 };
 boost::random::mt19937 RandomEngine::mTwister;
 
+template<typename Vector>
+void BindGlmVectorOp(sol::usertype<Vector>& vec)
+{
+    vec.set_function(sol::meta_function::addition, [](const Vector& a, const Vector& b) {
+        return a + b;
+    });
+    vec.set_function(sol::meta_function::subtraction, [](const Vector& a, const Vector& b) {
+        return a - b;
+    });
+    vec.set_function(sol::meta_function::multiplication, [](const Vector& vector, float scalar) {
+        return vector * scalar;
+    });
+    vec.set_function(sol::meta_function::multiplication, [](float scalar, const Vector& vector) {
+        return vector * scalar;
+    });
+    vec.set_function(sol::meta_function::division, [](const Vector& vector, float scalar) {
+        return vector / scalar;
+    });
+    vec.set_function(sol::meta_function::to_string, [](const Vector& vector) {
+        return base::ToString(vector);
+    });
+    vec["length"]    = [](const Vector& vec) { return glm::length(vec); };
+    vec["normalize"] = [](const Vector& vec) { return glm::normalize(vec); };
+}
+
 } // namespace
 
 namespace engine
@@ -947,44 +972,59 @@ void BindBase(sol::state& L)
 
 void BindGLM(sol::state& L)
 {
-    sol::constructors<glm::vec2(), glm::vec2(float, float)> ctors;
     auto glm  = L.create_named_table("glm");
-    auto vec2 = glm.new_usertype<glm::vec2>("vec2", ctors,
+
+    auto vec2 = glm.new_usertype<glm::vec2>("vec2",
+        sol::constructors<glm::vec2(), glm::vec2(float, float)>(),
         sol::meta_function::index, [](const glm::vec2& vec, int index) {
-        if (index >= 2)
-            throw std::runtime_error("glm.vec2 access out of bounds");
-        return vec[index];
-            });
-    vec2["x"]         = &glm::vec2::x;
-    vec2["y"]         = &glm::vec2::y;
-    vec2["length"]    = [](const glm::vec2& vec) { return glm::length(vec); };
-    vec2["normalize"] = [](const glm::vec2& vec) { return glm::normalize(vec); };
-    // adding meta functions for operators
-    // setting the index through set_function seems to be broken! (sol 3.2.3)
-    //type.set_function(sol::meta_function::index, [](const glm::vec2& vec, int index) {
-    //    return vec[index];
-    //});
-    vec2.set_function(sol::meta_function::addition, [](const glm::vec2& a, const glm::vec2& b) {
-        return a + b;
-    });
-    vec2.set_function(sol::meta_function::subtraction, [](const glm::vec2& a, const glm::vec2& b) {
-        return a - b;
-    });
-    vec2.set_function(sol::meta_function::multiplication, [](const glm::vec2& vector, float scalar) {
-        return vector * scalar;
-    });
-    vec2.set_function(sol::meta_function::multiplication, [](float scalar, const glm::vec2& vector) {
-        return vector * scalar;
-    });
-    vec2.set_function(sol::meta_function::division, [](const glm::vec2& vector, float scalar) {
-        return vector / scalar;
-    });
-    vec2.set_function(sol::meta_function::to_string, [](const glm::vec2& vector) {
-        return std::to_string(vector.x) + ", " + std::to_string(vector.y);
-    });
-    glm["dot"]       = [](const glm::vec2 &a, const glm::vec2 &b) { return glm::dot(a, b); };
-    glm["length"]    = [](const glm::vec2& vec) { return glm::length(vec); };
-    glm["normalize"] = [](const glm::vec2& vec) { return glm::normalize(vec); };
+            if (index >= 2)
+                throw std::runtime_error("glm.vec2 access out of bounds");
+            return vec[index];
+            }
+    );
+    vec2["x"] = &glm::vec2::x;
+    vec2["y"] = &glm::vec2::y;
+    BindGlmVectorOp<glm::vec2>(vec2);
+
+    auto vec3 = glm.new_usertype<glm::vec3>("vec3",
+        sol::constructors<glm::vec3(), glm::vec3(float, float, float)>(),
+        sol::meta_function::index, [](const glm::vec3& vec, int index) {
+                if (index >= 3)
+                    throw std::runtime_error("glm.vec3 access out of bounds");
+                return vec[index];
+            }
+    );
+    vec3["x"] = &glm::vec3::x;
+    vec3["y"] = &glm::vec3::y;
+    vec3["z"] = &glm::vec3::z;
+    BindGlmVectorOp<glm::vec3>(vec3);
+
+    auto vec4 = glm.new_usertype<glm::vec4>("vec4",
+        sol::constructors<glm::vec4(), glm::vec4(float, float, float, float)>(),
+        sol::meta_function::index, [](const glm::vec4& vec, int index) {
+                if (index >= 4)
+                    throw std::runtime_error("glm.vec4 access out of bounds");
+                return vec[index];
+            }
+    );
+    vec4["x"] = &glm::vec4::x;
+    vec4["y"] = &glm::vec4::y;
+    vec4["z"] = &glm::vec4::z;
+    vec4["w"] = &glm::vec4::w;
+    BindGlmVectorOp<glm::vec4>(vec4);
+
+    glm["dot"] = sol::overload(
+            [](const glm::vec2& a, const glm::vec2& b) { return glm::dot(a, b); },
+            [](const glm::vec3& a, const glm::vec3& b) { return glm::dot(a, b); },
+            [](const glm::vec4& a, const glm::vec4& b) { return glm::dot(a, b); });
+    glm["length"] = sol::overload(
+            [](const glm::vec2& vec) { return glm::length(vec); },
+            [](const glm::vec3& vec) { return glm::length(vec); },
+            [](const glm::vec4& vec) { return glm::length(vec); });
+    glm["normalize"] = sol::overload(
+            [](const glm::vec2& vec) { return glm::normalize(vec); },
+            [](const glm::vec3& vec) { return glm::normalize(vec); },
+            [](const glm::vec4& vec) { return glm::normalize(vec); });
 
     auto mat4 = glm.new_usertype<glm::mat4>("mat4");
     mat4["decompose"] = [](const glm::mat4& mat) {
