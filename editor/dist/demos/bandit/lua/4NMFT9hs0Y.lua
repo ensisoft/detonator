b77e525a3cc3682c6ebd4a5d44d224039f9a9315
Sami Väisänen <samiv@ensisoft.com>
2022-02-21 22:22:03 +0100

    Add basic gameplay and other game elements to Bandit demo
    Add basic gameplay and other game elements to Bandit demo:
    - Add main menu and in-game UIs.
    - Basic game state machinery to go from UI to game play and to
      handle player death, lives, respawn and level completion
    - Spawn coin particles on collect.
    - Kill objects at bottom scene boundary.
    - Make the stone blocks fall down after player trigger.
    - Control player movement on arrow keys.
    - Define game play objective (collect diamond) and track coins.
    - Update the demo screenshot.

---
+++ editor/dist/demos/bandit/lua/4NMFT9hs0Y.lua
@@ -0,0 +1,67 @@
+-- Entity 'StoneBlock' script.
+
+-- This script will be called for every instance of 'StoneBlock'
+-- in the scene during gameplay.
+-- You're free to delete functions you don't need.
+
+-- Called when the game play begins for an entity in the scene.
+function BeginPlay(stoneblock, scene)
+
+end
+
+-- Called when the game play ends for an entity in the scene.
+function EndPlay(stoneblock, scene)
+
+end
+
+-- Called on every low frequency game tick.
+function Tick(stoneblock, game_time, dt)
+
+end
+
+-- Called on every iteration of the game loop.
+function Update(stoneblock, game_time, dt)
+
+end
+
+-- Called on every iteration of the game loop game
+-- after *all* entities have been updated.
+function PostUpdate(stoneblock, game_time)
+end
+
+-- Called on collision events with other objects.
+function OnBeginContact(stoneblock, node, other, other_node)
+    if other:GetClassName() == 'Player' then 
+        local body = node:GetRigidBody()
+        stoneblock:PlayAnimationByName('Fall Down')
+    end
+end
+
+-- Called on collision events with other objects.
+function OnEndContact(stoneblock, node, other, other_node)
+end
+
+-- Called on key down events.
+function OnKeyDown(stoneblock, symbol, modifier_bits)
+end
+
+-- Called on key up events.
+function OnKeyUp(stoneblock, symbol, modifier_bits)
+end
+
+-- Called on mouse button press events.
+function OnMousePress(stoneblock, mouse)
+end
+
+-- Called on mouse button release events.
+function OnMouseRelease(stoneblock, mouse)
+end
+
+-- Called on mouse move events.
+function OnMouseMove(stoneblock, mouse)
+end
+
+-- Called on game events.
+function OnGameEvent(stoneblock, event)
+end
+
--- editor/dist/demos/bandit/lua/game.lua
+++ editor/dist/demos/bandit/lua/game.lua
@@ -7,30 +7,73 @@
 -- 'Game' is the native instance of *this* game that is running. It provides
 --        the native services for interfacing with the game engine.
 
--- the current scene that we're playing.
-Scene  = nil
 -- viewport into the game world, we move it as the game
 -- advances to follow the player
 Viewport = base.FRect:new(0.0, 0.0, 1200, 800.0)
 
-function SetTimeScale(entity, node_name, scale)
+_States = {
+    Menu,
+    Play
+}
+
+_State = _States.Menu
+_Lives = 2
+
+function _SetTimeScale(entity, node_name, scale)
     local node = entity:FindNodeByClassName(node_name)
     local draw = node:GetDrawable()
     draw:SetTimeScale(scale)
+end
+
+function _SetBackgroundVelocity(velocity)
+    local bg = Scene:FindEntityByInstanceName('Background')
+    _SetTimeScale(bg, 'Layer0', velocity * 1.0)
+    _SetTimeScale(bg, 'Layer1', velocity * 1.0)
+    _SetTimeScale(bg, 'Layer2', velocity * 1.0)
+end
+
+function _GameOver()
+    Game:CloseUI(0)
+    Game:Play('Menu Scene')
+    Game:OpenUI('Menu UI')
+    Game:SetViewport(-600, -400, 1200, 800)
+end
+
+function _RespawnLevel()
+    Game:Delay(1.0)
+    Game:Play('Level 0')
 end
 
 -- main game callbacks
 
 -- start the game and show some initial content.
 function StartGame()
-    local scene = ClassLib:FindSceneClassByName('My Scene')
-    Game:Play(scene)
-    Game:SetViewport(Viewport)
+    Game:SetViewport(-600, -400, 1200, 800)
+    Game:Play('Menu Scene')
+    Game:OpenUI('Menu UI')
 end
 
 -- Called when the scene begins to play
 function BeginPlay(scene)
-    Scene  = scene
+    if scene:GetClassName() == 'Level 0' then 
+        local ui = Game:GetTopUI()
+        if ui == nil then 
+            return 
+        end
+        local l0 = ui:FindWidgetByName('life0', 'Label')
+        local l1 = ui:FindWidgetByName('life1', 'Label')
+        Game:DebugPrint(tostring(l1))
+        if _Lives == 2 then 
+           l0:SetVisible(true)
+           l1:SetVisible(true)
+        elseif _Lives == 1 then
+            l0:SetVisible(false)
+            l1:SetVisible(true)
+        elseif _Lives == 0 then
+            l0:SetVisible(false)
+            l1:SetVisible(false)
+        end                
+    end
 end
 
 -- Called when the scene play ends.
@@ -43,10 +86,17 @@ function Tick(game_time, dt)
 end
 
 function Update(game_time, dt)
+    if Scene == nil then 
+        return 
+    end
     local player = Scene:FindEntityByInstanceName('Player')
-    if player == nil then
+    if player == nil or player.died then 
         return
     end
+
+    local ui = Game:GetTopUI()
+    local coins = ui:FindWidgetByName('coins', 'Label')
+    coins:SetText('x' .. tostring(player.coins))
 
     local node = player:FindNodeByClassName('Body')
     local pos  = node:GetTranslation()
@@ -55,10 +105,8 @@ function Update(game_time, dt)
 
     -- adjust the scrolling speed of the background layers by
     -- adjusting the time scale of the background layer drawables
-    local bg = Scene:FindEntityByInstanceName('Background')
-    SetTimeScale(bg, 'Layer0', velo.x * 1.0)
-    SetTimeScale(bg, 'Layer1', velo.x * 1.0)
-    SetTimeScale(bg, 'Layer2', velo.x * 1.0)
+    _SetBackgroundVelocity(velo.x)
+
 
     -- move the game viewport to follow the player.
     local viewport_offset = glm.vec2:new(200, 600)
@@ -81,8 +129,9 @@ end
 -- input event handlers
 function OnKeyDown(symbol, modifier_bits)
     if symbol == wdk.Keys.Escape then
-        local scene = ClassLib:FindSceneClassByName('My Scene')
-        Game:Play(scene)
+        if _State == _States.Menu then 
+            Game:Quit(0)
+        end
     end
 end
 
@@ -90,3 +139,35 @@ function OnKeyUp(symbol, modifier_bits)
 
 end
 
+function OnGameEvent(event)
+    if event.from == 'player' and event.message == 'died' then 
+        Game:DebugPrint('Player died')
+        if Scene:GetClassName() == 'Menu Scene' then 
+            Game:Play('Menu Scene')
+        else
+            _SetBackgroundVelocity(0.0)
+            _Lives = _Lives - 1
+            if (_Lives == -1) then 
+                _GameOver()
+                return
+            end
+            _RespawnLevel()
+        end
+    elseif event.from == 'player' and event.message == 'level-complete' then
+        Game:DebugPrint('Level complete')
+        _GameOver()
+    end
+end
+
+function OnUIAction(ui, action)
+    if action.name == 'quit' then
+        Game:Quit(0)
+    elseif action.name == 'play' then 
+        _State = _States.Play
+        _Lives = 2
+        Game:CloseUI(0)
+        Game:Play('Level 0')
+        Game:OpenUI('Game UI')
+        
+    end
+end
