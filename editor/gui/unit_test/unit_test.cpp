951cb3562c0f21af7999668828bc03276949d316
Sami Väisänen <samiv@ensisoft.com>
2022-09-28 23:07:12 +0200

    Fix awesome Qt JSON/Variant derp again
    Fix awesome Qt JSON/Variant derp again. On x86-64 size_t is 64bits
    which silently goes into QVariant which goes into QJsonDocument
    but silently borks.
    
    Fixing this bug also fixes several bugs regarding the state of the
    mainwidgets and whether the system thinks there are changes that
    need to be saved. These bugs happened because of using size_t for
    hashing and the hash value would not be correctly restored on
    session load. Barf.
    
    Add a unit test for testing some more of the code in the editor/gui
    in the future.

---
+++ editor/gui/unit_test/unit_test.cpp
@@ -0,0 +1,63 @@
+// Copyright (C) 2020-2022 Sami Väisänen
+// Copyright (C) 2020-2022 Ensisoft http://www.ensisoft.com
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+#include "config.h"
+
+#include "warnpush.h"
+#include "warnpop.h"
+
+#include "base/test_minimal.h"
+#include "editor/gui/settings.h"
+
+void unit_test_settings_json()
+{
+    {
+        gui::Settings s("gui-settings.json");
+        s.SetValue("foo", "string", QString("jeesus ajaa mopolla"));
+        s.SetValue("foo", "int", int(123));
+        s.SetValue("foo", "float", float(1.0f));
+        s.SetValue("foo", "utf8-string", std::string("joo joo €€"));
+        s.SetValue("foo", "uint64", size_t(0xffffffffffffffff));
+        TEST_REQUIRE(s.Save());
+    }
+
+    {
+        gui::Settings s("gui-settings.json");
+        TEST_REQUIRE(s.Load());
+        QString str;
+        TEST_REQUIRE(s.GetValue("foo", "string", &str));
+        TEST_REQUIRE(str == "jeesus ajaa mopolla");
+        int value;
+        TEST_REQUIRE(s.GetValue("foo", "int", &value));
+        TEST_REQUIRE(value == 123);
+        float flt;
+        TEST_REQUIRE(s.GetValue("foo", "float", &flt));
+        TEST_REQUIRE(flt == 1.0f);
+        std::string utf8;
+        TEST_REQUIRE(s.GetValue("foo", "utf8-string", &utf8));
+        TEST_REQUIRE(utf8 == "joo joo €€");
+        size_t val;
+        TEST_REQUIRE(s.GetValue("foo", "uint64", &val));
+        TEST_REQUIRE(val == size_t(0xffffffffffffffff));
+    }
+}
+
+int test_main(int argc, char* argv[])
+{
+    unit_test_settings_json();
+
+    return 0;
+}
