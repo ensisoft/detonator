e4b03a44524b0fd62e52258919db7c7cfded8c4a
Sami Väisänen <samiv@ensisoft.com>
2022-11-07 19:30:46 +0100

    Remove MSAA4 setting from unit_test_device
    Remove MSAA4 setting from unit_test_device as a quick fix for failing
    to find a matching OpenGL ES config on Win10 with Nvidia Optimus 516.54.

--- graphics/unit_test/unit_test_device.cpp
+++ graphics/unit_test/unit_test_device.cpp
@@ -50,11 +50,10 @@ public:
         attrs.stencil_size = 8;
         attrs.surfaces.pbuffer = true;
         attrs.double_buffer = false;
-        attrs.sampling = wdk::Config::Multisampling::MSAA4;
         attrs.srgb_buffer = true;
+        constexpr auto debug_context = false;
         mConfig   = std::make_unique<wdk::Config>(attrs);
-        mContext  = std::make_unique<wdk::Context>(*mConfig, 2, 0,  false, //debug
-            wdk::Context::Type::OpenGL_ES);
+        mContext  = std::make_unique<wdk::Context>(*mConfig, 2, 0, debug_context, wdk::Context::Type::OpenGL_ES);
         mSurface  = std::make_unique<wdk::Surface>(*mConfig, w, h);
         mContext->MakeCurrent(mSurface.get());
     }
