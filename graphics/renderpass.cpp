234aae55d0ad6aca2b2ee461d56edc9e8c7434a5
Sami Väisänen <samiv@ensisoft.com>
2023-01-28 10:46:33 +0100

    Add low level graphics render passes and refactor painter draw methods
    Add low level graphics render passes and refactor painter draw methods.
    This change is needed because there's a need to be able to leverage
    the painter draw methods in more elaborate drawing systems when doing
    multiple passes over the items and rendering to various different
    render targets/buffers. This change splits some rendering state into
    "render pass"  objects which are then responsible for setting up the
    right device state for writing to color/depth/stencil buffers. Doing
    this also simplifies the painter implementation since it no longer
    needs to implement different draws such as mask+cover itself but the
    caller can create such a setup easily. The legacy functionality for
    the simplest draw is kept for backwards compatibility by adding some
    default render pass implementations and using those implicitly.
    
    The render pass object is now also added to the environment arguments
    passed to the material and drawable when applying program/device state.
    This allows the materials and drawables to provide alternative ways
    for performing some render pass, such as using simpler shaders when
    doing a stencil pass and now actual RGB computation is needed.

---
+++ graphics/renderpass.cpp
@@ -0,0 +1,54 @@
+// Copyright (C) 2020-2023 Sami Väisänen
+// Copyright (C) 2020-2023 Ensisoft http://www.ensisoft.com
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+
+#include "config.h"
+
+#include "graphics/renderpass.h"
+#include "graphics/device.h"
+
+namespace gfx {
+namespace detail {
+void GenericRenderPass::Begin(Device& device, State* state) const
+{
+    state->write_color  = true;
+    state->stencil_func = StencilFunc::Disabled;
+    state->depth_test   = DepthTest::Disabled;
+}
+
+void StencilMaskPass::Begin(Device& device, State* state) const
+{
+    device.ClearStencil(mStencilClearValue);
+    state->write_color   = false;
+    state->depth_test    = DepthTest::Disabled;
+    state->stencil_func  = StencilFunc::PassAlways;
+    state->stencil_dpass = StencilOp::WriteRef;
+    state->stencil_dfail = StencilOp::WriteRef;
+    state->stencil_ref   = mStencilWriteValue;
+    state->stencil_mask  = 0xff;
+}
+
+void StencilTestColorWritePass::Begin(Device& device, State* state) const
+{
+    state->write_color   = true;
+    state->depth_test    = DepthTest::Disabled;
+    state->stencil_func  = StencilFunc::RefIsEqual;
+    state->stencil_dpass = StencilOp::DontModify;
+    state->stencil_dfail = StencilOp::DontModify;
+    state->stencil_ref   = mStencilRefValue;
+    state->stencil_mask  = 0xff;
+}
+
+} // namespace
+} // namespace
