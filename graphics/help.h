ed44871afb9650f7b20ec80d88acf62f0da14a14
Sami Väisänen <samiv@ensisoft.com>
2023-02-18 12:27:18 +0100

    Add some graphics utility functions to graphics/help.h|.cpp
    Add some graphics utility functions to graphics/help.h|.cpp.

---
+++ graphics/help.h
@@ -0,0 +1,39 @@
+// Copyright (C) 2020-2023 Sami Väisänen
+// Copyright (C) 2020-2023 Ensisoft http://www.ensisoft.com
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+#pragma once
+
+#include "config.h"
+
+#include <string>
+
+// low level helpers
+
+namespace gfx
+{
+class Program;
+class Device;
+class Geometry;
+
+Program* MakeProgram(const std::string& vertex_source,
+                     const std::string& fragment_source,
+                     const std::string& program_name,
+                     Device& device);
+
+Geometry* MakeFullscreenQuad(Device& device);
+
+} // namespace
+
