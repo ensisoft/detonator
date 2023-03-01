312c01241b4ea23093df453fcf8005a8094ed3d5
Sami Väisänen <samiv@ensisoft.com>
2023-03-01 22:52:23 +0100

    Update the font map dialog instructions
    Update the font map dialog instructions.

--- editor/gui/dlgfontmap.cpp
+++ editor/gui/dlgfontmap.cpp
@@ -299,13 +299,12 @@ void DlgFontMap::OnPaintScene(gfx::Painter& painter, double secs)
         ShowInstruction(
             "INSTRUCTIONS\n"
             "1. Select pre-generated font character texture map.\n"
-            "2. Assign key presses to each character.\n",
+            "2. Assign key presses to each character.\n"
+            "3. Click on 'Export' to export the font JSON.\n",
             gfx::FRect(0, 0, width, height),
             painter);
         return;
     }
-
-
 
     const float zoom   = GetValue(mUI.zoom);
     const float img_width  = mWidth * zoom;
