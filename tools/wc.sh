1b0b35d5b55314b7346f138e453da0cb6b0f60b5
Sami Väisänen <samiv@ensisoft.com>
2023-03-04 09:49:16 +0100

    Add some tools and programming tests under tools and tools/test
    Add some tools and programming tests under tools and tools/test.
    - Move the Lua/sol2 programming test here.
    - Add a glm/math programming test application for doing some glm,
      math and linear algebra test programming.
    - Add a simple line counting shell script for estimating project LOC.
    - Add a minimal python http server, which enables some magic hTTP
      headers so that JS SharedArrayBuffer works so that Emscripten
      threads can work. *sigh*

---
+++ tools/wc.sh
@@ -0,0 +1,20 @@
+wc -l `find base/ -name *.h` \
+`find base/ -name *.cpp` \
+`find audio/ -name *.h` \
+`find audio/ -name *.cpp` \
+`find data/ -name *.h` \
+`find data/ -name *.cpp` \
+`find editor/ -name *.h` \
+`find editor/ -name *.cpp` \
+`find emscripten/ -name *.h` \
+`find emscripten/ -name *.cpp` \
+`find engine/ -name *.h` \
+`find engine/ -name *.cpp` \
+`find game/ -name *.h` \
+`find game/ -name *.cpp` \
+`find graphics/ -name *.h` \
+`find graphics/ -name *.cpp` \
+`find uikit/ -name *.h` \
+`find uikit/ -name *.cpp` \
+`find wdk/wdk/ -name *.h` \
+`find wdk/wdk/ -name *.cpp` \
