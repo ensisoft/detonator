398b71d378439e6be3f8b624c4b421646b10a381
Sami Väisänen <samiv@ensisoft.com>
2023-03-16 16:46:18 +0100

    Add noexcept exception specification to base/types.h
    Add noexcept exception specification to base/types.h.

--- base/types.h
+++ base/types.h
@@ -28,20 +28,20 @@ namespace base
     {
     public:
         Size() = default;
-        Size(T width, T height)
+        Size(T width, T height) noexcept
           : mWidth(width)
           , mHeight(height)
         {}
         template<typename F> explicit
-        Size(const Size<F>& other)
+        Size(const Size<F>& other) noexcept
           : mWidth(other.GetWidth())
           , mHeight(other.GetHeight())
         {}
-        T GetWidth() const { return mWidth; }
-        T GetHeight() const { return mHeight; }
+        T GetWidth() const noexcept { return mWidth; }
+        T GetHeight() const noexcept { return mHeight; }
 
         // returns true if either dimension is 0.
-        bool IsZero() const
+        bool IsZero() const noexcept
         {
             const bool has_width  = mWidth != T();
             const bool has_height = mHeight != T();
@@ -54,19 +54,19 @@ namespace base
         T mHeight = T();
     };
     template<typename T> inline
-    Size<T> operator*(const Size<T>& size, T scale)
+    Size<T> operator*(const Size<T>& size, T scale) noexcept
     {
         return Size<T>(size.GetWidth() * scale,
                        size.GetHeight() * scale);
     }
     template<typename T> inline
-    Size<T> operator+(const Size<T>& rhs, const Size<T>& lhs)
+    Size<T> operator+(const Size<T>& rhs, const Size<T>& lhs) noexcept
     {
         return Size<T>(rhs.GetWidth() + lhs.GetWidth(),
                        rhs.GetHeight() + lhs.GetHeight());
     }
     template<typename T> inline
-    Size<T> operator-(const Size<T>& rhs, const Size<T>& lhs)
+    Size<T> operator-(const Size<T>& rhs, const Size<T>& lhs) noexcept
     {
         return Size<T>(rhs.GetWidth() - lhs.GetWidth(),
                        rhs.GetHeight() - lhs.GetHeight());
@@ -100,26 +100,26 @@ namespace base
     {
     public:
         Point() = default;
-        Point(T x, T y)
+        Point(T x, T y) noexcept
           : mX(x)
           , mY(y)
         {}
         template<typename F> explicit
-        Point(const Point<F>& other)
+        Point(const Point<F>& other) noexcept
           : mX(other.GetX())
           , mY(other.GetY())
         {}
 
-        T GetX() const { return mX; }
-        T GetY() const { return mY; }
+        T GetX() const noexcept { return mX; }
+        T GetY() const noexcept { return mY; }
 
-        Point& operator+=(const Point& other)
+        Point& operator+=(const Point& other) noexcept
         {
             mX += other.mX;
             mY += other.mY;
             return *this;
         }
-        Point& operator-=(const Point& other)
+        Point& operator-=(const Point& other) noexcept
         {
             mX -= other.mX;
             mY -= other.mY;
@@ -131,13 +131,13 @@ namespace base
     };
 
     template<typename T> inline
-    Point<T> operator-(const Point<T>& lhs, const Point<T>& rhs)
+    Point<T> operator-(const Point<T>& lhs, const Point<T>& rhs) noexcept
     {
         return Point<T>(lhs.GetX() - rhs.GetX(),
                         lhs.GetY() - rhs.GetY());
     }
     template<typename T> inline
-    Point<T> operator+(const Point<T>& lhs, const Point<T>& rhs)
+    Point<T> operator+(const Point<T>& lhs, const Point<T>& rhs) noexcept
     {
         return Point<T>(lhs.GetX() + rhs.GetX(),
                         lhs.GetY() + rhs.GetY());
@@ -175,34 +175,34 @@ namespace base
         using SizeT  = Size<T>;
 
         Rect() = default;
-        Rect(T x, T y, T width, T height)
+        Rect(T x, T y, T width, T height) noexcept
           : mX(x)
           , mY(y)
           , mWidth(width)
           , mHeight(height)
         {}
         template<typename F> explicit
-        Rect(const Rect<F>& other)
+        Rect(const Rect<F>& other) noexcept
         {
             mX = static_cast<T>(other.GetX());
             mY = static_cast<T>(other.GetY());
             mWidth = static_cast<T>(other.GetWidth());
             mHeight = static_cast<T>(other.GetHeight());
         }
-        Rect(T x, T y, const Size<T>& size)
+        Rect(T x, T y, const Size<T>& size) noexcept
           : mX(x)
           , mY(y)
           , mWidth(size.GetWidth())
           , mHeight(size.GetHeight())
         {}
-        Rect(const Point<T>& pos, T width, T height)
+        Rect(const Point<T>& pos, T width, T height) noexcept
         {
             mX = pos.GetX();
             mY = pos.GetY();
             mWidth = width;
             mHeight = height;
         }
-        Rect(const Point<T>& pos, const Size<T>& size)
+        Rect(const Point<T>& pos, const Size<T>& size) noexcept
         {
             mX = pos.GetX();
             mY = pos.GetY();
@@ -210,65 +210,65 @@ namespace base
             mHeight = size.GetHeight();
         }
 
-        T GetHeight() const
+        T GetHeight() const noexcept
         { return mHeight; }
-        T GetWidth() const
+        T GetWidth() const noexcept
         { return mWidth; }
-        T GetX() const
+        T GetX() const noexcept
         { return mX; }
-        T GetY() const
+        T GetY() const noexcept
         { return mY; }
-        Point<T> GetPosition() const
+        Point<T> GetPosition() const noexcept
         { return Point<T>(mX, mY); }
-        Size<T> GetSize() const
+        Size<T> GetSize() const noexcept
         { return Size<T>(mWidth, mHeight); }
-        void SetX(T value)
+        void SetX(T value) noexcept
         { mX = value; }
-        void SetY(T value)
+        void SetY(T value) noexcept
         { mY = value; }
-        void SetWidth(T width)
+        void SetWidth(T width) noexcept
         { mWidth = width; }
-        void SetHeight(T height)
+        void SetHeight(T height) noexcept
         { mHeight = height; }
-        void Resize(T width, T height)
+        void Resize(T width, T height) noexcept
         {
             mWidth = width;
             mHeight = height;
         }
-        void Resize(const Size<T>& size)
+        void Resize(const Size<T>& size) noexcept
         {
             Resize(size.GetWidth(), size.GetHeight());
         }
-        void Grow(T some_width, T some_height)
+        void Grow(T some_width, T some_height) noexcept
         {
             mWidth += some_width;
             mHeight += some_height;
         }
-        void Grow(const Size<T>& some_size)
+        void Grow(const Size<T>& some_size) noexcept
         {
             Grow(some_size.GetWidth(), some_size.GetHeight());
         }
-        void Move(T x, T y)
+        void Move(T x, T y) noexcept
         {
             mX = x;
             mY = y;
         }
-        void Move(const Point<T>& pos)
+        void Move(const Point<T>& pos) noexcept
         {
             mX = pos.GetX();
             mY = pos.GetY();
         }
-        void Translate(T x, T y)
+        void Translate(T x, T y) noexcept
         {
             mX += x;
             mY += y;
         }
-        void Translate(const Point<T>& pos)
+        void Translate(const Point<T>& pos) noexcept
         {
             mX += pos.GetX();
             mY += pos.GetY();
         }
-        bool IsEmpty() const
+        bool IsEmpty() const noexcept
         {
             const bool has_width  = mWidth != T();
             const bool has_height = mHeight != T();
@@ -276,7 +276,7 @@ namespace base
         }
 
         // Return true if the point p is within this rectangle.
-        bool TestPoint(const Point<T>& p) const
+        bool TestPoint(const Point<T>& p) const noexcept
         {
             if (p.GetX() < mX || p.GetY() < mY)
                 return false;
@@ -284,13 +284,15 @@ namespace base
                 return false;
             return true;
         }
-        inline bool TestPoint(T x, T y) const
-        { return TestPoint(Point<T>(x, y)); }
+        bool TestPoint(T x, T y) const noexcept
+        {
+            return TestPoint(Point<T>(x, y));
+        }
 
         // Map a local point relative to the rect origin
         // into a global point relative to the origin of the
         // coordinate system.
-        Point<T> MapToGlobal(T x, T y) const
+        Point<T> MapToGlobal(T x, T y) const noexcept
         {
             return Point<T>(mX + x, mY + y);
         }
@@ -298,7 +300,7 @@ namespace base
         // Map a local point relative to the rect origin
         // into a global point relative to the origin of the
         // coordinate system
-        Point<T> MapToGlobal(const Point<T>& p) const
+        Point<T> MapToGlobal(const Point<T>& p) const noexcept
         {
             return Point<T>(mX + p.GetX(), mY + p.GetY());
         }
@@ -306,21 +308,21 @@ namespace base
         // Map a global point relative to the origin of the
         // coordinate system to a local point relative to the
         // origin of the rect.
-        Point<T> MapToLocal(T x, T y) const
+        Point<T> MapToLocal(T x, T y) const noexcept
         {
             return Point<T>(x - mX, y - mY);
         }
         // Map a global point relative to the origin of the
         // coordinate system to a local point relative to the
         // origin of the rect.
-        Point<T> MapToLocal(const Point<T>& p) const
+        Point<T> MapToLocal(const Point<T>& p) const noexcept
         {
             return Point<T>(p.GetX() - mX, p.GetY() - mY);
         }
 
         // Normalize the rectangle with respect to the given
         // dimensions.
-        Rect<float> Normalize(const Size<float>& space) const
+        Rect<float> Normalize(const Size<float>& space) const noexcept
         {
             const float x = mX / space.GetWidth();
             const float y = mY / space.GetHeight();
@@ -332,7 +334,7 @@ namespace base
         // Inverse of Normalize. Assuming a normalized rectangle
         // expand it to a non-normalized rectangle given the dimensions.
         template<typename F>
-        Rect<F> Expand(const Size<F>& space) const
+        Rect<F> Expand(const Size<F>& space) const noexcept
         {
             // floats or ints.
             const F x = mX * space.GetWidth();
@@ -346,7 +348,7 @@ namespace base
         // dimensions. Since the return rect is expressed with
         // unsigned types the rectangle cannot have a negative
         // x,y coord and such negative coords will be clamped to 0, 0
-        Rect<unsigned> Expand(const Size<unsigned>& space) const
+        Rect<unsigned> Expand(const Size<unsigned>& space) const noexcept
         {
             // float to unsigned can be undefined conversion if the
             // value cannot be represented in the destination type.
@@ -359,7 +361,7 @@ namespace base
             return Rect<unsigned>(x, y, w, h);
         }
         // Split the rect into 4 sub-quadrants.
-        std::tuple<Rect, Rect, Rect, Rect> GetQuadrants() const
+        std::tuple<Rect, Rect, Rect, Rect> GetQuadrants() const noexcept
         {
             const auto half = T(2);
             const auto half_width = mWidth/half;
@@ -371,7 +373,7 @@ namespace base
             return {r0, r1, r2, r3};
         }
         // Get the 4 corners of the rectangle.
-        std::tuple<PointT, PointT, PointT, PointT> GetCorners() const
+        std::tuple<PointT, PointT, PointT, PointT> GetCorners() const noexcept
         {
             const PointT p0(mX, mY);
             const PointT p1(mX, mY+mHeight);
@@ -379,7 +381,7 @@ namespace base
             const PointT p3(mX+mWidth, mY+mHeight);
             return {p0, p1, p2, p3};
         }
-        PointT GetCenter() const
+        PointT GetCenter() const noexcept
         {
             const auto half = T(2);
             return PointT(mX + mWidth/half, mY + mHeight/half);
@@ -422,7 +424,7 @@ namespace base
 
     // Test whether the rectangle A contains rectangle B completely.
     template<typename T>
-    bool Contains(const Rect<T>& a, const Rect<T>& b)
+    bool Contains(const Rect<T>& a, const Rect<T>& b) noexcept
     {
         // if all the corners of B are within A then B
         // is completely within A
@@ -436,7 +438,7 @@ namespace base
     // Find the intersection of the two rectangles
     // and return the rectangle that represents the intersection.
     template<typename T>
-    Rect<T> Intersect(const Rect<T>& lhs, const Rect<T>& rhs)
+    Rect<T> Intersect(const Rect<T>& lhs, const Rect<T>& rhs) noexcept
     {
         using R = Rect<T>;
         if (lhs.IsEmpty() || rhs.IsEmpty())
@@ -451,13 +453,10 @@ namespace base
         const auto rhs_right  = rhs_left + rhs.GetWidth();
         const auto rhs_bottom = rhs_top + rhs.GetHeight();
         // no intersection conditions
-        if (lhs_right < rhs_left)
-            return R();
-        else if (lhs_left > rhs_right)
-            return R();
-        else if (lhs_top > rhs_bottom)
-            return R();
-        else if (lhs_bottom < rhs_top)
+        if (lhs_right < rhs_left  ||
+            lhs_left > rhs_right ||
+            lhs_top > rhs_bottom ||
+            lhs_bottom < rhs_top)
             return R();
 
         const auto right  = std::min(lhs_right, rhs_right);
@@ -468,7 +467,7 @@ namespace base
     }
 
     template<typename T>
-    bool DoesIntersect(const Rect<T>& lhs, const Rect<T>& rhs)
+    bool DoesIntersect(const Rect<T>& lhs, const Rect<T>& rhs) noexcept
     {
         using R = Rect<T>;
         if (lhs.IsEmpty() || rhs.IsEmpty())
@@ -492,7 +491,7 @@ namespace base
     }
 
     template<typename T>
-    Rect<T> Union(const Rect<T>& lhs, const Rect<T>& rhs)
+    Rect<T> Union(const Rect<T>& lhs, const Rect<T>& rhs) noexcept
     {
         using R = Rect<T>;
         if (lhs.IsEmpty())
