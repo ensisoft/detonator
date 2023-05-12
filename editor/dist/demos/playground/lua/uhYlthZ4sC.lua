789471e27cead71650e655108548f285c70bc214
Sami Väisänen <samiv@ensisoft.com>
2023-05-12 10:30:12 +0200

    Update playground demo with line segment spatial index query
    Update playground demo with line segment spatial index query.

--- editor/dist/demos/playground/lua/uhYlthZ4sC.lua
+++ editor/dist/demos/playground/lua/uhYlthZ4sC.lua
@@ -2,18 +2,51 @@
 -- This script will be called for every instance of 'Spatial Query Test'
 -- during gameplay.
 -- You're free to delete functions you don't need.
+-- 
+-- LuaFormatter off
+
 local mouse_down = nil
 local mouse_pos = nil
 
-local SelectionMode = {
+-- The method by which the objects are queried for.
+local SelectionMethod = {
+    -- Find objects whose bounding boxes intersect with the
+    -- search rectangle. 
     Rectangle = 'Rectangle',
-    AllPoint = 'AllPoints',
-    AllPointWithRadius = 'AllPointsWithRadius',
-    ClosestPoint = 'ClosestPoint',
-    ClosestPointWithRadius = 'ClosestPointWithRadius'
+    -- Find objects whose bounding boxes intersect (contain)
+    -- the search point. 
+    Point = 'Point',
+    -- Find objects whose bounding boxes intersect (contain)
+    -- any point within the given radius from the search point.
+    -- In other words, this is an intersection test against a rect
+    -- and a circle, where the circle is defined by point+radius.
+    PointRadius = 'PointRadius',
+    -- Find objects whose bounding boxes intersect with the with 
+    -- the line defined by two points.
+    PointPoint = 'PointPoint'
 }
 
-local selection_mode = SelectionMode.Rectangle
+-- The selection mode only applies to point and point+radius
+-- object queries.
+local SelectionMode = {
+    -- Find all objects that match the query criteria.
+    All = 'All',
+    -- Find the first object that matches the query criteria.
+    -- The order in which the objects are evaluated is arbitrary,
+    -- thus the result is whatever object happened to be match first.
+    First = 'First',
+    -- Find the closest object among the objects that matched the
+    -- query criteria. The distance is measured to the center of the
+    -- object's bounding box. For point-point queries the distnce is
+    -- is measured between the starting point (point A) and the 
+    -- center of the bounding box.
+    Closest = 'Closest'
+}
+
+-- LuaFormatter on
+
+local selection_mode = SelectionMode.All
+local selection_method = SelectionMethod.Rectangle
 
 -- Called when the scene begins play.
 function BeginPlay(spatial_query_test)
@@ -40,17 +73,27 @@ end
 
 -- Called on every iteration of game loop.
 function Update(spatial_query_test, game_time, dt)
+    local pink = base.Color4f.FromEnum('HotPink')
+
+    if selection_method == SelectionMethod.Point then
+        if mouse_pos == nil then
+            return
+        end
+        Game:DebugDrawCircle(mouse_pos, 4.0, pink, 2.0)
+    end
+
     if mouse_down == nil or mouse_pos == nil then
         return
     end
 
-    local pink = base.Color4f.FromEnum('HotPink')
-
-    if selection_mode == SelectionMode.Rectangle then
+    if selection_method == SelectionMethod.Rectangle then
         Game:DebugDrawRect(mouse_down, mouse_pos, pink, 2.0)
-    elseif selection_mode == SelectionMode.AllPointWithRadius then
+    elseif selection_method == SelectionMethod.PointRadius then
+        local radius = glm.length(mouse_down - mouse_pos)
+
+        Game:DebugDrawCircle(mouse_down, radius, pink, 2.0)
         Game:DebugDrawLine(mouse_down, mouse_pos, pink, 2.0)
-    elseif selection_mode == SelectionMode.ClosestPointWithRadius then
+    elseif selection_method == SelectionMethod.PointPoint then
         Game:DebugDrawLine(mouse_down, mouse_pos, pink, 2.0)
     end
 end
@@ -67,17 +110,23 @@ end
 -- Called on key down events.
 function OnKeyDown(spatial_query_test, symbol, modifier_bits)
     if symbol == wdk.Keys.Key1 then
-        selection_mode = SelectionMode.Rectangle
+        selection_method = SelectionMethod.Rectangle
     elseif symbol == wdk.Keys.Key2 then
-        selection_mode = SelectionMode.AllPoint
+        selection_method = SelectionMethod.Point
     elseif symbol == wdk.Keys.Key3 then
-        selection_mode = SelectionMode.AllPointWithRadius
+        selection_method = SelectionMethod.PointRadius
     elseif symbol == wdk.Keys.Key4 then
-        selection_mode = SelectionMode.ClosestPoint
-    elseif symbol == wdk.Keys.Key5 then
-        selection_mode = SelectionMode.ClosestPointWithRadius
+        selection_method = SelectionMethod.PointPoint
+    elseif symbol == wdk.Keys.KeyA then
+        selection_mode = SelectionMode.All
+    elseif symbol == wdk.Keys.KeyF then
+        selection_mode = SelectionMode.First
+    elseif symbol == wdk.Keys.KeyC then
+        selection_mode = SelectionMode.Closest
     end
-    Game:DebugPrint('Selection mode ' .. tostring(selection_mode))
+    Game:DebugPrint('Selection method ' .. selection_method)
+    Game:DebugPrint('Selection mode ' .. selection_mode)
+
 end
 
 -- Called on key up events.
@@ -104,28 +153,27 @@ function OnMouseRelease(spatial_query_test, mouse)
     local height = max_y - min_y
 
     local nodes = nil
-    if selection_mode == SelectionMode.Rectangle then
+    if selection_method == SelectionMethod.Rectangle then
         nodes = Scene:QuerySpatialNodes(base.FRect:new(min_x, min_y, width,
                                                        height))
-    elseif selection_mode == SelectionMode.AllPoint then
-        nodes = Scene:QuerySpatialNodes(mouse_pos, 'All')
-    elseif selection_mode == SelectionMode.AllPointWithRadius then
+    elseif selection_method == SelectionMethod.Point then
+        nodes = Scene:QuerySpatialNodes(mouse_pos, selection_mode)
+    elseif selection_method == SelectionMethod.PointRadius then
         local radius = glm.length(mouse_pos - mouse_down)
-        nodes = Scene:QuerySpatialNodes(mouse_down, radius, 'All')
-    elseif selection_mode == SelectionMode.ClosestPoint then
-        nodes = Scene:QuerySpatialNodes(mouse_down, 'Closest')
-    elseif selection_mode == SelectionMode.ClosestPointWithRadius then
-        local radius = glm.length(mouse_pos - mouse_down)
-        nodes = Scene:QuerySpatialNodes(mouse_down, radius, 'Closest')
+        nodes = Scene:QuerySpatialNodes(mouse_down, radius, selection_mode)
+    elseif selection_method == SelectionMethod.PointPoint then
+        nodes = Scene:QuerySpatialNodes(mouse_down, mouse_pos, selection_mode)
     end
 
     while (nodes:HasNext()) do
         local node = nodes:GetNext()
-        local args = game.EntityArgs:new()
-        args.class = ClassLib:FindEntityClassByName('Marker')
-        args.position = node:GetTranslation()
-        args.layer = 2
-        Scene:SpawnEntity(args, true)
+        local pos = node:GetTranslation()
+
+        Scene:SpawnEntity('Marker', {
+            pos = pos,
+            layer = 2,
+            link = true
+        })
     end
 
     mouse_pos = nil
