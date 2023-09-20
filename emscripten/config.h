5c2f07d6a11c6da33602548420b7341d2a934ee0
Sami Väisänen <samiv@ensisoft.com>
2023-09-20 14:05:42 +0200

    Update documentation regarding threading in Emscripten/WASM/web
    Update documentation regarding threading in Emscripten/WASM/web.

--- emscripten/config.h
+++ emscripten/config.h
@@ -4,8 +4,26 @@
 #include "base/platform.h"
 
 #define AUDIO_USE_OPENAL
-// disable player threading, it doesn't seem to work right.
-//#define AUDIO_USE_PLAYER_THREAD
+
+// When this flag is defined the OpenAL API calls are checked for errors.
+// Currently turned off for performance reasons. Might cause unexpected
+// issues and errors that are not discovered properly.
+//
+//#define AUDIO_CHECK_OPENAL
+
+// When this flag is defined the audio player in audio/player.h uses a
+// separate std::thread to run the platform specific audio device and
+// to manage the audio playback.
+//
+// #define AUDIO_USE_PLAYER_THREAD
+
+
+// When AUDIO_USE_PLAYER_THREAD is defined  use this flag to control
+// the type of locking around the audio queue inside the audio/player
+// When the flag is defined the audio queue is a lock free queue
+// otherwise a standard queue with std mutex is used.
+#define AUDIO_USE_LOCK_FREE_QUEUE
+
 #define BASE_LOGGING_ENABLE_LOG
 #define BASE_FORMAT_SUPPORT_GLM
 #define BASE_FORMAT_SUPPORT_MAGIC_ENUM
@@ -16,6 +34,5 @@
 
 // keep these turned off on purpose for now
 //#define GRAPHICS_CHECK_OPENGL
-//#define AUDIO_CHECK_OPENAL
 
 #define SOL_ALL_SAFETIES_ON 1
