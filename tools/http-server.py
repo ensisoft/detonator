0d0bf68ff6bd23803a39cfebf0b9abfaae3cf49f
Sami Väisänen <samiv@ensisoft.com>
2024-01-25 21:49:52 +0100

    Fix the http-server.py script content type setting
    Fix the http-server.py script content type setting. Adding a content
    type header in the end_headers() method actually ends up adding
    the header *again*. The body of the http-server has already added
    some Content-Type header !
    
    This causes confusion in some browsers !
    
    See this bug report:
    
    https://github.com/emscripten-core/emscripten/issues/12473
    
    A better solution seems to be overriding the guess_type for
    guessing the content type.

--- tools/http-server.py
+++ tools/http-server.py
@@ -1,6 +1,7 @@
 import http.server
 import socketserver
 import os
+import time
 
 # Set the port you want to use for the web server
 port = 8000
@@ -8,13 +9,21 @@ port = 8000
 # Define a custom request handler to enable SharedArrayBuffer
 class CustomRequestHandler(http.server.SimpleHTTPRequestHandler):
     def end_headers(self):
-        if self.path.endswith(".js"):
-            print('set content type to text/javascript')
-            self.send_header('Content-Type', 'text/javascript')
+        #if self.path.endswith(".js"):
+        #    print('set content type to text/javascript')
+        #    self.send_header('Content-Type', 'text/javascript')
 
         self.send_header('Cross-Origin-Opener-Policy', 'same-origin')
         self.send_header('Cross-Origin-Embedder-Policy', 'require-corp')
         super().end_headers()
+
+    def guess_type(self, path):
+        if path.endswith('.html'):
+            return "text/html"
+        if path.endswith('.js'):
+            return "text/javascript"
+
+        return super().guess_type(path)
 
 # Change to the directory where your files are located
 web_dir = "."
