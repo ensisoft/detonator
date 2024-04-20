7457cf799cf8e3897fce49013786167bd4f9d767
Sami Väisänen <samiv@ensisoft.com>
2024-04-20 15:38:24 +0200

    Add TimeWidget to the Qt designer plugin build
    Add TimeWidget to the Qt designer plugin build.

--- editor/gui/qt/plugin.h
+++ editor/gui/qt/plugin.h
@@ -61,6 +61,32 @@ namespace gui {
     };
 } // gui
 
+
+class TimeWidgetPlugin : public QObject, public QDesignerCustomWidgetInterface
+{
+Q_OBJECT
+    Q_INTERFACES(QDesignerCustomWidgetInterface)
+
+public:
+    TimeWidgetPlugin(QObject* parent = nullptr);
+
+    virtual bool isContainer() const override;
+    virtual bool isInitialized() const override;
+    virtual QIcon icon() const override;
+    virtual QString domXml() const override;
+    virtual QString group() const override;
+    virtual QString includeFile() const override;
+    virtual QString name() const override;
+    virtual QString toolTip() const override;
+    virtual QString whatsThis() const override;
+    virtual QWidget *createWidget(QWidget *parent) override;
+    virtual void initialize(QDesignerFormEditorInterface *core) override;
+
+private:
+    bool initialized = false;
+};
+
+
 class CurveWidgetPlugin : public QObject, public QDesignerCustomWidgetInterface
 {
 Q_OBJECT
