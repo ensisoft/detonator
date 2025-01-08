ed2500c86300b5a13fc25e0d657586c099f094d0
Sami Väisänen <samiv@ensisoft.com>
2025-01-08 21:54:50 +0100

    Fix basic light material "color" output
    Fix basic light material "color" output.

--- graphics/shaders/fragment_basic_light_material_shader.glsl
+++ graphics/shaders/fragment_basic_light_material_shader.glsl
@@ -34,7 +34,7 @@ void FragmentShaderMain() {
     vec4 diffuse_color = kDiffuseColor;
     vec4 specular_color = kSpecularColor;
 
-    if (TestMap(LIGHT_DIFFUSE_MAP)) {
+    if (TestMap(BASIC_LIGHT_MATERIAL_DIFFUSE_MAP)) {
         vec2 diffuse_map_coords = vTexCoord * kDiffuseMapRect.zw + kDiffuseMapRect.xy;
         vec4 diffuse_map_sample = texture(kDiffuseMap, diffuse_map_coords);
         // using the color to modulate the texture samplling
@@ -42,21 +42,21 @@ void FragmentShaderMain() {
         ambient_color *= diffuse_map_sample;
     }
 
-    if (TestMap(LIGHT_SPECULAR_MAP)) {
+    if (TestMap(BASIC_LIGHT_MATERIAL_SPECULAR_MAP)) {
         vec2 specular_map_coords = vTexCoord * kSpecularMapRect.zw + kSpecularMapRect.xy;
         vec4 specular_map_sample = texture(kSpecularMap, specular_map_coords);
         // using the color to modulate the texture sampling
         specular_color *= specular_map_sample;
     }
 
-    if (TestMap(LIGHT_NORMAL_MAP)) {
+    if (TestMap(BASIC_LIGHT_MATERIAL_NORMAL_MAP)) {
         vec2 normal_map_coords = vTexCoord * kNormalMapRect.zw + kNormalMapRect.xy;
         vec4 normal_map_sample = texture(kNormalMap, normal_map_coords);
         fs_out.surface_normal = normalize(normal_map_sample.xyz * 2.0 - 1.0);
         fs_out.have_surface_normal = true;
     }
 
-    fs_out.color = kDiffuseColor;
+    fs_out.color = diffuse_color;
     fs_out.ambient_color = ambient_color;
     fs_out.diffuse_color = diffuse_color;
     fs_out.specular_color = specular_color;
