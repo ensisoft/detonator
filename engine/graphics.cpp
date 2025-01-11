c667aa56654c467a162cef4e3b7f148df0c036a3
Sami Väisänen <samiv@ensisoft.com>
2025-01-11 23:35:38 +0100

    Refactor engine graphics shader program to be independent
    Refactor engine graphics shader program to be independent and not
    to rely on the base class. Copy paste implementation from graphics/
    so that the base class can be refactored.

--- engine/graphics.cpp
+++ engine/graphics.cpp
@@ -57,6 +57,55 @@ public:
         ret += "+main";
         return ret;
     }
+    std::string GetShaderId(const gfx::Drawable& drawable, const gfx::Drawable::Environment& env) const override
+    {
+        auto ret = drawable.GetShaderId(env);
+        ret += "+main";
+        return ret;
+    }
+
+    gfx::ShaderSource GetShader(const gfx::Drawable& drawable, const gfx::Drawable::Environment& env, const gfx::Device& device) const
+    {
+        gfx::ShaderSource source;
+        source.SetType(gfx::ShaderSource::Type::Vertex);
+        source.LoadRawSource(R"(
+#version 300 es
+
+struct VS_OUT {
+    // vertex position in clip space (after projection transformation)
+    vec4 clip_position;
+    // vertx position in eye coordinates (after camera/view transformation)
+    vec4 view_position;
+    // view space surface normal vector
+    vec3 view_normal;
+    // view space surface tagent vector
+    vec3 view_tangent;
+    // view space surface bi-tangent vector
+    vec3 view_bitangent;
+    // point size for GL_POINTS rasterization.
+    float point_size;
+
+    bool need_tbn;
+    bool have_tbn;
+} vs_out;
+
+void VertexShaderMain();
+
+void main() {
+    vs_out.have_tbn = false;
+    vs_out.need_tbn = false;
+    VertexShaderMain();
+
+    gl_PointSize = vs_out.point_size;
+    gl_Position  = vs_out.clip_position;
+}
+)");
+
+        source.Merge(drawable.GetShader(env, device));
+        return source;
+    }
+
+
     gfx::ShaderSource GetShader(const gfx::Material& material, const gfx::Material::Environment& env, const gfx::Device& device) const override
     {
         gfx::ShaderSource source;
@@ -64,7 +113,7 @@ public:
         source.SetVersion(gfx::ShaderSource::Version::GLSL_300);
         source.SetPrecision(gfx::ShaderSource::Precision::High);
 
-        source.AddSource(R"(
+        source.LoadRawSource(R"(
 #define SHADER_FLAG_ENABLE_BLOOM 1
 
 struct FS_OUT {
