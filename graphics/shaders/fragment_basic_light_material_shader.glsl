d2fae2d0f232e36a43f21f9de67e480f04c26e80
Sami Väisänen <samiv@ensisoft.com>
2025-01-26 11:36:15 +0100

    Add point rendering support to basic light material shader
    Add point rendering support to basic light material shader. Use the
    base shader functionality to read texture coordinates either from
    vTexCoord varying or from gl_PointCoord depending on the rendering
    primitive.

--- graphics/shaders/fragment_basic_light_material_shader.glsl
+++ graphics/shaders/fragment_basic_light_material_shader.glsl
@@ -22,9 +22,6 @@ uniform vec4 kNormalMapRect;
 
 uniform uint kMaterialMaps;
 
-// @varyings
-in vec2 vTexCoord;
-
 bool TestMap(uint map) {
     return ((kMaterialMaps & map) == map);
 }
@@ -35,8 +32,10 @@ void FragmentShaderMain() {
     vec4 diffuse_color = kDiffuseColor;
     vec4 specular_color = kSpecularColor;
 
+    vec2 texture_coords = GetTextureCoords();
+
     if (TestMap(BASIC_LIGHT_MATERIAL_DIFFUSE_MAP)) {
-        vec2 diffuse_map_coords = vTexCoord * kDiffuseMapRect.zw + kDiffuseMapRect.xy;
+        vec2 diffuse_map_coords = texture_coords * kDiffuseMapRect.zw + kDiffuseMapRect.xy;
         vec4 diffuse_map_sample = texture(kDiffuseMap, diffuse_map_coords);
         // using the color to modulate the texture samplling
         diffuse_color *= diffuse_map_sample;
@@ -44,14 +43,14 @@ void FragmentShaderMain() {
     }
 
     if (TestMap(BASIC_LIGHT_MATERIAL_SPECULAR_MAP)) {
-        vec2 specular_map_coords = vTexCoord * kSpecularMapRect.zw + kSpecularMapRect.xy;
+        vec2 specular_map_coords = texture_coords * kSpecularMapRect.zw + kSpecularMapRect.xy;
         vec4 specular_map_sample = texture(kSpecularMap, specular_map_coords);
         // using the color to modulate the texture sampling
         specular_color *= specular_map_sample;
     }
 
     if (TestMap(BASIC_LIGHT_MATERIAL_NORMAL_MAP)) {
-        vec2 normal_map_coords = vTexCoord * kNormalMapRect.zw + kNormalMapRect.xy;
+        vec2 normal_map_coords = texture_coords * kNormalMapRect.zw + kNormalMapRect.xy;
         vec4 normal_map_sample = texture(kNormalMap, normal_map_coords);
         fs_out.surface_normal = normalize(normal_map_sample.xyz * 2.0 - 1.0);
         fs_out.have_surface_normal = true;
