437c57cbfb4213cfe1fd656d65502d0f43064bfa
Sami Väisänen <samiv@ensisoft.com>
2025-05-17 12:07:21 +0200

    Reformat tilebatch vertex shaders
    Reformat tilebatch vertex shaders.

--- graphics/shaders/vertex_tilebatch_quad_shader.glsl
+++ graphics/shaders/vertex_tilebatch_quad_shader.glsl
@@ -21,22 +21,22 @@ out vec2 vTileData;
 out vec2 vTexCoord;
 
 void VertexShaderMain() {
-  // transform tile col,row index into a tile position in tile world units in the tile x,y plane
-  vec3 tile = aTilePosition * kTileWorldSize + kTilePointOffset;
+    // transform tile col,row index into a tile position in tile world units in the tile x,y plane
+    vec3 tile = aTilePosition * kTileWorldSize + kTilePointOffset;
 
-  // transform the tile from tile space to rendering space
-  vec4 vertex = kTileCoordinateSpaceTransform * vec4(tile.xyz, 1.0);
+    // transform the tile from tile space to rendering space
+    vec4 vertex = kTileCoordinateSpaceTransform * vec4(tile.xyz, 1.0);
 
-  // pull the corner vertices apart by adding a corner offset
-  // for each vertex towards some corner/direction away from the
-  // center point
-  vertex.xy += (aTileCorner * kTileRenderSize);
+    // pull the corner vertices apart by adding a corner offset
+    // for each vertex towards some corner/direction away from the
+    // center point
+    vertex.xy += (aTileCorner * kTileRenderSize);
 
-  vs_out.clip_position = kTileTransform * vertex;
-  vs_out.clip_position.z = 0.0;
+    vs_out.clip_position = kTileTransform * vertex;
+    vs_out.clip_position.z = 0.0;
 
-  vTexCoord = aTileCorner + vec2(0.5, 1.0);
-  vTileData = aTileData;
+    vTexCoord = aTileCorner + vec2(0.5, 1.0);
+    vTileData = aTileData;
 }
 
 )CPP_RAW_STRING"
