e27ddfd24da6f147f5d32d9f7b9d4b2b1e9a6972
Sami Väisänen <samiv@ensisoft.com>
2025-05-24 09:43:18 +0200

    Add basic light support to tilebatch quad and point vertex shaders
    Add basic light support to tilebatch quad and point vertex shaders.

--- graphics/shaders/vertex_tilebatch_point_shader.glsl
+++ graphics/shaders/vertex_tilebatch_point_shader.glsl
@@ -26,10 +26,26 @@ void VertexShaderMain() {
     vec4 vertex = kTileCoordinateSpaceTransform * vec4(tile.xyz, 1.0);
 
     vec4 view_vertex = kTileViewTransform * vertex;
+    view_vertex.z = 0.0;
 
+    vs_out.view_position = view_vertex;
     vs_out.clip_position = kProjectionMatrix * view_vertex;
     vs_out.clip_position.z = 0.0;
     vs_out.point_size = kTileRenderSize.x;
+
+    if (vs_out.need_tbn) {
+        // with point rendering we can simply hardcode the
+        // tangent vectors since the rasterized surface is always
+        // parallel to the projection plane and in view coordinates
+        // the only thing changes is the translation
+        const vec3 normal = vec3(0.0, 0.0, 1.0);
+        const vec3 tangent = vec3(1.0, 0.0, 0.0);
+        const vec3 bitangent = vec3(0.0, 1.0, 0.0);
+        vs_out.view_normal    = normal;
+        vs_out.view_tangent   = tangent;
+        vs_out.view_bitangent = bitangent;
+        vs_out.have_tbn = true;
+    }
 
     vTileData = aTileData;
 }
--- graphics/shaders/vertex_tilebatch_quad_shader.glsl
+++ graphics/shaders/vertex_tilebatch_quad_shader.glsl
@@ -34,9 +34,25 @@ void VertexShaderMain() {
     vertex.xy += (aTileCorner * kTileRenderSize);
 
     vec4 view_vertex = kTileViewTransform * vertex;
+    view_vertex.z = 0.0;
 
+    vs_out.view_position = view_vertex;
     vs_out.clip_position = kProjectionMatrix * view_vertex;
     vs_out.clip_position.z = 0.0;
+
+    if (vs_out.need_tbn) {
+        // with point rendering we can simply hardcode the
+        // tangent vectors since the rasterized surface is always
+        // parallel to the projection plane and in view coordinates
+        // the only thing changes is the translation
+        const vec3 normal = vec3(0.0, 0.0, 1.0);
+        const vec3 tangent = vec3(1.0, 0.0, 0.0);
+        const vec3 bitangent = vec3(0.0, 1.0, 0.0);
+        vs_out.view_normal    = normal;
+        vs_out.view_tangent   = tangent;
+        vs_out.view_bitangent = bitangent;
+        vs_out.have_tbn = true;
+    }
 
     vTexCoord = aTileCorner + vec2(0.5, 1.0);
     vTileData = aTileData;
