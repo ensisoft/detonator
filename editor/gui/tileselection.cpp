d105c715022543bbda1804b0e555cc7d8c8c3642
Sami Väisänen <samiv@ensisoft.com>
2025-05-31 12:10:27 +0200

    Add finishing touches to the tile selection refactoring
    Add finishing touches to the tile selection refactoring.

--- editor/gui/tileselection.cpp
+++ editor/gui/tileselection.cpp
@@ -21,18 +21,18 @@ namespace gui
 {
 
 TileSelection::TileSelection(unsigned col, unsigned row, unsigned width, unsigned height)
-  : start_row(row)
-  , start_col(col)
-  , width(width)
-  , height(height)
+  : mStartRow(row)
+  , mStartCol(col)
+  , mWidth(width)
+  , mHeight(height)
 {
     for (unsigned x=0; x<width; ++x)
     {
         for (unsigned y=0; y<height; ++y)
         {
             TileSelection::Tile tile;
-            tile.x = start_col + x;
-            tile.y = start_row + y;
+            tile.x = mStartCol + x;
+            tile.y = mStartRow + y;
             mTiles.push_back(tile);
         }
     }
@@ -45,9 +45,9 @@ TileSelection::TileSelection(const base::URect& rect)
 
 bool TileSelection::IsSelected(unsigned tile_xpos, unsigned tile_ypos) const
 {
-    if (tile_xpos < start_col ||  tile_xpos >= start_col + width)
+    if (tile_xpos < mStartCol || tile_xpos >= mStartCol + mWidth)
         return false;
-    if (tile_ypos < start_row || tile_ypos >= start_row + height)
+    if (tile_ypos < mStartRow || tile_ypos >= mStartRow + mHeight)
         return false;
 
     for (const auto& tile : mTiles)
@@ -65,27 +65,27 @@ bool TileSelection::IsSelected(const Tile& tile) const
 
 bool TileSelection::DisjointSelection() const
 {
-    if (mTiles.size() != width * height)
+    if (mTiles.size() != mWidth * mHeight)
         return true;
     return false;
 }
 
 bool TileSelection::IsEmpty() const
 {
-    return width == 0 || height == 0;
+    return mWidth == 0 || mHeight == 0;
 }
 
 bool TileSelection::HasSelection() const
 {
-    return width != 0 && height != 0;
+    return mWidth != 0 && mHeight != 0;
 }
 
 void TileSelection::Clear()
 {
-    width = 0;
-    height = 0;
-    start_col = 0;
-    start_row = 0;
+    mWidth = 0;
+    mHeight = 0;
+    mStartCol = 0;
+    mStartRow = 0;
     mTiles.clear();
 }
 
@@ -110,10 +110,10 @@ void TileSelection::Deselect(const Tile& tile)
 
 bool TileSelection::ShiftSelection(int dx, int dy, unsigned map_width, unsigned map_height)
 {
-    const auto start_col = static_cast<int64_t>(this->start_col);
-    const auto start_row = static_cast<int64_t>(this->start_row);
-    const auto width     = static_cast<int64_t>(this->width);
-    const auto height    = static_cast<int64_t>(this->height);
+    const auto start_col = static_cast<int64_t>(mStartCol);
+    const auto start_row = static_cast<int64_t>(mStartRow);
+    const auto width     = static_cast<int64_t>(mWidth);
+    const auto height    = static_cast<int64_t>(mHeight);
 
     if (start_col + dx < 0 || start_col + width + dx > map_width)
         dx = 0;
@@ -124,8 +124,8 @@ bool TileSelection::ShiftSelection(int dx, int dy, unsigned map_width, unsigned 
     if (dx == 0 && dy == 0)
         return false;
 
-    this->start_row = start_row + dy;
-    this->start_col = start_col + dx;
+    mStartRow = start_row + dy;
+    mStartCol = start_col + dx;
 
     for (auto& tile : mTiles)
     {
@@ -133,8 +133,8 @@ bool TileSelection::ShiftSelection(int dx, int dy, unsigned map_width, unsigned 
         tile.y += dy;
     }
 
-    ASSERT(this->start_col + this->width <= map_width);
-    ASSERT(this->start_row + this->height <= map_height);
+    ASSERT(mStartCol + mWidth <= map_width);
+    ASSERT(mStartRow + mHeight <= map_height);
     return true;
 }
 
@@ -151,10 +151,10 @@ TileSelection TileSelection::Combine(const TileSelection& one, const TileSelecti
     ASSERT(one.resolution == two.resolution);
 
     TileSelection ret;
-    ret.start_col = rect.GetX();
-    ret.start_row = rect.GetY();
-    ret.width = rect.GetWidth();
-    ret.height = rect.GetHeight();
+    ret.mStartCol = rect.GetX();
+    ret.mStartRow = rect.GetY();
+    ret.mWidth = rect.GetWidth();
+    ret.mHeight = rect.GetHeight();
     ret.mTiles = one.mTiles;
     ret.resolution = one.resolution;
 
