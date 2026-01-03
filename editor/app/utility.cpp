925fce57e9695b92b1703460e41abcf481578a4f
Sami Väisänen <samiv@ensisoft.com>
2026-01-03 11:35:02 +0100

    Add detonator2 editor QSS style
    Add detonator2 editor QSS style a variant of the previous detonator
    QSS theme but a little bit darker.

--- editor/app/utility.cpp
+++ editor/app/utility.cpp
@@ -376,7 +376,8 @@ bool SetTheme(const QString& name)
 bool SetStyle(const QString& name)
 {
 #if defined(DETONATOR_EDITOR_BUILD) || defined(DETONATOR_GAMEHOST_BUILD)
-    if (name == "Fusion-Dark" || name == "DETONATOR" || name == "Cyber Punk")
+    if (name == "Fusion-Dark" || name == "DETONATOR" || name == "DETONATOR2" ||
+        name == "Cyber Punk")
     {
         auto* style = new DarkStyle();
         style->setProperty("style", name);
