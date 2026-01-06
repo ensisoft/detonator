505ffe65a558f6d30aa4d0981de73cb68c6be75f
Sami Väisänen <samiv@ensisoft.com>
2026-01-06 10:37:34 +0100

    Add graphics PaintContext for recording info (errors) during painting
    Add graphics PaintContext for recording info (errors) during painting.
    Any error encountered inside the main loop when painting is tricky
    because:
    
    - If we don't log the error, the information is lost and user doesn't
      know that something is failing.
    - If we DO log the error the error log gets flooded and spammed on
      every iteration of the loop as long as the error condition persists.
      This spamming slows everything down and makes the log less useful
      because of the flood of repeated information overtakes all other
      information.
    
    It's a catch 22 really. Damned if you do, damned if you don't.
    
    So the idea here is that for these errors instead of recording them
    in the standard log we capture them in a paint context specific log
    and at some higher level (where the paint operation really began)
    we can then unwind the errors and paint them to the screen instead
    of spamming them in the log.
    
    The PaintContext is implicitly stackable so that various paint
    operations can easily be nested. A PaintContext is meant to only
    exist for some short duration and be created on the stack.If there's
    no current paint context then we fallback for logging/spamming the
    base log as before.

---
+++ graphics/paint_log.h
@@ -0,0 +1,52 @@
+// Copyright (C) 2020-2026 Sami Väisänen
+// Copyright (C) 2020-2026 Ensisoft http://www.ensisoft.com
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+#pragma once
+
+#include "config.h"
+
+#include "graphics/paint_context.h"
+
+namespace gfx
+{
+    void WritePaintContextLogMessage(PaintContext::LogEvent type, const char* file, int line, std::string message);
+
+    // Interface for writing a variable argument message to the
+    // calling thread's logger or the global logger.
+    template<typename... Args>
+    void WritePaintContextLog(PaintContext::LogEvent type, const char* file, int line, const std::string& fmt, const Args&... args)
+    {
+        // format the message in the log statement.
+        WritePaintContextLogMessage(type, file, line, base::FormatString(fmt, args...));
+    }
+
+    // these are drop in replacement macros for the base logging.
+    // the idea is that we can easily capture the errors and store
+    // them in the context (if any). Anything that is in the render
+    // loop should use these macros instead of the base macros.
+#define GFX_PAINT_VERBOSE(fmt, ...) WritePaintContextLog(PaintContext::LogEvent::Verbose, __FILE__, __LINE__, fmt, ## __VA_ARGS__)
+#define GFX_PAINT_DEBUG(fmt, ...)   WritePaintContextLog(PaintContext::LogEvent::Debug,   __FILE__, __LINE__, fmt, ## __VA_ARGS__)
+#define GFX_PAINT_WARN(fmt, ...)    WritePaintContextLog(PaintContext::LogEvent::Warning, __FILE__, __LINE__, fmt, ## __VA_ARGS__)
+#define GFX_PAINT_INFO(fmt, ...)    WritePaintContextLog(PaintContext::LogEvent::Info,    __FILE__, __LINE__, fmt, ## __VA_ARGS__)
+#define GFX_PAINT_ERROR(fmt, ...)   WritePaintContextLog(PaintContext::LogEvent::Error,   __FILE__, __LINE__, fmt, ## __VA_ARGS__)
+
+#define GFX_PAINT_ERROR_RETURN(ret, fmt, ...) \
+    do { \
+        WritePaintContextLog(PaintContext::LogEvent::Error, __FILE__, __LINE__, fmt, ## __VA_ARGS__); \
+        return ret; \
+    } while (0)
+
+} // namespace
