1083b2d2329bae3922885ee6a90f8a4718edaacc
Sami Väisänen <samiv@ensisoft.com>
2026-03-29 21:03:54 +0200

    Refactor 3D shape and 3D drawing tests to feature tests
    Refactor 3D shape and 3D drawing tests to feature tests in
    graphics test app.

---
+++ graphics/test/dist/issue-missing-gold.sh
@@ -0,0 +1,6 @@
+./graphics_test --test --issue-missing-gold
+./graphics_test --test --issue-missing-gold --msaa4
+./graphics_test --test --issue-missing-gold --msaa8
+./graphics_test --test --issue-missing-gold --es3
+./graphics_test --test --issue-missing-gold --es3 --msaa4
+./graphics_test --test --issue-missing-gold --es3 --msaa8
